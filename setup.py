c4b1b04c25899801b942c1ddf178d29d6d16fbda
Josh Izaac <josh146@gmail.com>
2018-11-20 17:17:43 -0500

    slight update

--- setup.py
+++ setup.py
@@ -121,7 +121,6 @@ classifiers = [
     "Operating System :: Microsoft :: Windows",
     "Programming Language :: Python",
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
