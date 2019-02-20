705d71258be5b70104a4f0c7ff45bf053a52b18f
bgupt <bgupt@gravity.psu.edu>
2019-02-20 10:53:30 -0500

    Modifying setup.py and torontonian source codes.

--- src/torontonian.F90
+++ src/torontonian.F90
@@ -12,56 +12,56 @@
 ! See the License for the specific language governing permissions and
 ! limitations under the License.
 
-! Module containing custom precision types
-module kinds
-    implicit none
-
-    integer, parameter :: sp = selected_real_kind(5,30)
-    integer, parameter :: dp = selected_real_kind(9,99)
-    integer, parameter :: qp = selected_real_kind(20, 199)
-    integer, parameter :: wp = qp
-    integer, parameter :: ip = selected_int_kind(16)
-
-    ! These empty arrays are used to initialize variables to either the min or
-    ! max possible number of kind wp or integer.
-    real(wp), dimension(2:1)    :: empty
-    integer(ip), dimension(2:1) :: iempty
-end module kinds
-
-
-! Module containing some common variables
-module vars
-    use kinds
-    implicit none
-
-    ! real(wp), dimension(:, :), allocatable    :: mat, inv_mat
-    real(wp), dimension(:, :), allocatable    :: mat_comp_real, mat_comp_imag
-    complex(wp), dimension(:, :), allocatable :: inv_mat_comp
-
-
-    complex(wp), parameter :: zzero = (0.0_wp, 0.0_wp), zone = (1.0_wp, 0.0_wp), zi = (0.0_wp, 1.0_wp)
-
-    character :: mattype*10
-
-end module vars
 
 
 module torontonian
     use kinds
     use vars
     use omp_lib
+    use ISO_FORTRAN_ENV
     use linpack_q_complex
 
     implicit none
 
     contains
 
-    function tor(mat)
-        complex(wp), intent(in) :: mat(:, :)
-        complex(wp) :: tor
+    subroutine hello(varin, varout)
+      use kinds
+   
+      real(dp), intent(in) :: varin
+      real(wp) :: tmp
+      real(dp), intent(out) :: varout
+
+      !f2py intent(in) :: varin
+      !f2py intent(out) :: varout
+
+      print*, 'varin=', varin
+      tmp = real(varin, wp)
+      varout = tmp!real(tmp, dp)
+      print*, 'varout=', varout
+ 
+      print*, "hello from Toronotnian!"
+    end subroutine hello
+
+    !function tor(mat)
+    subroutine tor(matin, tor_out)
+      use kinds
+      use vars
+      use omp_lib
+      use ISO_FORTRAN_ENV
+
+      implicit none
+
+        complex(dp), intent(in) :: matin(:, :)
+        complex(wp), allocatable :: mat(:, :)
+        complex(dp), allocatable :: mat_dp1(:, :), mat_dp2(:, :), mat_dp3(:, :)
+        !complex(wp) :: tor
+        complex(dp), intent(out) :: tor_out
+        real(dp) :: tmp, tmp2, tmp3
+        real(wp) :: tmpqp
 
         ! local variables
-        integer(ip) :: n, i, j, k, total, cntr, ell, ii
+        integer(ip) :: n, i, j, k, total, cntr, ell, ii, nthreads
         complex(wp) :: tmpsum_complex, invdet_complex
 
         ! result variables
@@ -77,9 +77,29 @@ module torontonian
         integer(ip), allocatable :: bin(:), iter(:), iter2(:)
 
 
-        !f2py intent(in) :: mat
+        !f2py intent(in) :: matin
+        !f2py intent(out) :: tor_out
 
-        n = size(mat(1,:))
+        n = size(matin(1,:))
+
+        allocate(mat(1:n, 1:n), mat_dp1(1:n, 1:n),mat_dp2(1:n, 1:n), mat_dp3(1:n, 1:n))
+
+        mat_dp1(:, :) = (0.0_dp, 0.0_dp)
+
+        print*, real(mat_dp1(1,1), wp)
+        print*, real(matin(1,1), wp) - real(real(matin(1,1)), dp)
+
+        forall (i=1:n, j=1:n) mat(i,j) = zone*real(matin(i,j), wp) + zi*real(aimag(matin(i,j)), wp)!matdp(i,j)!(real(matdp(i,j), wp), aimag(matdp(i,j), wp))
+
+        forall (i=1:n, j=1:n) mat_dp1(i, j) = (1.0_dp, 0.0_dp)*real(matin(i,j), dp) + (0.0_dp, 1.0_dp)*real(aimag(matin(i,j)), dp)
+
+        tmp = real(mat(1,1), dp)
+        tmp2 = mat(1,1) - real(tmp, wp)
+        tmp3 = (mat(1,1) - real(tmp, wp)) - real(tmp2, wp)
+        tmpqp = real(tmp, wp) + real(tmp2, wp) + real(tmp3, wp)
+
+        print*, tmp, tmp2, tmp3
+        print*, tmpqp 
 
         ell = n/2
 
@@ -120,13 +140,16 @@ module torontonian
             call qgedi_complex (submat_comp, lda, nn, ipvt, det_complex, work_complex, job )
             invdet_complex = zone/det_complex(1) * 10.0_wp**(-real(det_complex(2)))
             tmpsum_complex = tmpsum_complex + (-1.0_wp)**(ell-total)*sqrt(invdet_complex)
+            !print*, tmpsum_complex
             deallocate(iter, iter2, submat_comp, ipvt, work_complex)
         end do
         !$OMP END PARALLEL DO
 
-        tor = tmpsum_complex
+        !tor = (1.0_wp, 0.0_wp)!tmpsum_complex
+        tor_out = tmpsum_complex
 
-    end function tor
+    end subroutine tor
+    !end function tor
 
     subroutine dec2bin (kk, nnn, matt, summ)
         integer(ip), intent(in)  :: kk, nnn
@@ -150,5 +173,7 @@ module torontonian
         summ = sum(matt(1:nnn))
 
     end subroutine dec2bin
+
+
 
 end module torontonian
