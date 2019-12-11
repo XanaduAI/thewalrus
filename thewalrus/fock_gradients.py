d6349fc7ec2ece09c123a544a3364902849e7c49
Nicolas Quesada <zeitus@gmail.com>
2019-12-10 19:15:55 -0500

    Moves all the gates to fock_gradient

--- thewalrus/fock_gradients.py
+++ thewalrus/fock_gradients.py
@@ -44,8 +44,214 @@ from thewalrus.quantum import fock_tensor
 from thewalrus.symplectic import squeezing, two_mode_squeezing, beam_splitter
 
 
+
+
+@jit("void(complex128[:,:], complex128[:,:], complex128[:,:], double)")
+def grad_Dgate(T, gradTr, gradTtheta, theta):  # pragma: no cover
+    """Calculates the gradient of the Dgate.
+
+    Args:
+        T (array[complex]): array representing the gate
+        gradTr (array[complex]): array of zeros that will contain the value of the gradient with respect to r
+        gradTr (array[complex]): array of zeros that will contain the value of the gradient with respect to theta
+        theta (float): phase angle parametrizing the gate
+    """
+    cutoff = gradTr.shape[0]
+    exptheta = np.exp(1j * theta)
+    for n in range(cutoff):
+        for m in range(cutoff):
+            gradTtheta[n, m] = 1j * (n - m) * T[n, m]
+            gradTr[n, m] = np.sqrt(m + 1) * T[n, m + 1] * exptheta
+            if m > 0:
+                gradTr[n, m] -= np.sqrt(m) * T[n, m - 1] * np.conj(exptheta)
+
+
+def Dgate(r, theta, cutoff, grad=False, s=np.arcsinh(1.0)):
+    """Calculates the Fock representation of the Dgate and its gradient.
+
+    Arg:
+        r (float): magnitude parameter of the gate
+        theta (float): magnitude parameter of the gate
+        cutoff (int): Fock ladder cutoff
+        grad (boolean): whether to calculate the gradient or not
+        r (float): value of the parameter used internally in fock_tensor
+
+    Returns:
+        tuple[array[complex], array[complex], array[complex]]: The Fock representations of the gate and its gradients with sizes ``[cutoff]*2``
+    """
+    nmodes = 1
+    alpha = np.array([r * np.exp(1j * theta)])
+    S = np.identity(2 * nmodes)
+
+    if not grad:
+        return fock_tensor(S, alpha, cutoff, r=s), None, None
+
+    T = fock_tensor(S, alpha, cutoff + 1, r=s)
+    gradTr = np.zeros([cutoff, cutoff], dtype=complex)
+    gradTtheta = np.zeros([cutoff, cutoff], dtype=complex)
+    grad_Dgate(T, gradTr, gradTtheta, theta)
+    return T[0:cutoff, 0:cutoff], gradTr, gradTtheta
+
+
+@jit("void(complex128[:,:], complex128[:,:], complex128[:,:], double)")
+def grad_Sgate(T, gradTr, gradTtheta, theta):  # pragma: no cover
+    """Calculates the gradient of the Sgate.
+
+    Args:
+        T (array[complex]): array representing the gate
+        gradTr (array[complex]): array of zeros that will contain the value of the gradient with respect to r
+        gradTr (array[complex]): array of zeros that will contain the value of the gradient with respect to theta
+        theta (float): phase angle parametrizing the gate
+    """
+    cutoff = gradTr.shape[0]
+    exptheta = np.exp(1j * theta)
+    for n in range(cutoff):
+        offset = n % 2
+        for m in range(offset, cutoff, 2):
+            gradTtheta[n, m] = 0.5j * (n - m) * T[n, m]
+            gradTr[n, m] = -0.5 * np.sqrt((m + 1) * (m + 2)) * T[n, m + 2] * exptheta
+            if m > 1:
+                gradTr[n, m] += 0.5 * np.sqrt(m * (m - 1)) * T[n, m - 2] * np.conj(exptheta)
+
+
+def Sgate(r, theta, cutoff, grad=False, s=np.arcsinh(1.0)):
+    """Calculates the Fock representation of the Sgate and its gradient.
+
+    Arg:
+        r (float): magnitude parameter of the gate
+        theta (float): magnitude parameter of the gate
+        cutoff (int): Fock ladder cutoff
+        grad (boolean): whether to calculate the gradient or not
+        r (float): value of the parameter used internally in fock_tensor
+
+    Returns:
+        tuple[array[complex], array[complex], array[complex]]: The Fock representations of the gate and its gradients with sizes ``[cutoff]*2``
+    """
+    alpha = np.array([0.0])
+    S = squeezing(r, theta)
+
+    if not grad:
+        return fock_tensor(S, alpha, cutoff, r=s), None, None
+
+    T = fock_tensor(S, alpha, cutoff + 2, r=s)
+    gradTr = np.zeros([cutoff, cutoff], dtype=complex)
+    gradTtheta = np.zeros([cutoff, cutoff], dtype=complex)
+    grad_Sgate(T, gradTr, gradTtheta, theta)
+    return T[0:cutoff, 0:cutoff], gradTr, gradTtheta
+
+
+@jit("void(complex128[:,:,:,:],complex128[:,:,:,:], complex128[:,:,:,:], double)")
+def grad_S2gate(T, gradTr, gradTtheta, theta):  # pragma: no cover
+    """Calculates the gradient of the S2gate.
+
+    Args:
+        T (array[complex]): array representing the gate
+        gradTr (array[complex]): array of zeros that will contain the value of the gradient with respect to r
+        gradTr (array[complex]): array of zeros that will contain the value of the gradient with respect to theta
+        theta (float): phase angle parametrizing the gate
+    """
+    cutoff = gradTr.shape[0]
+    exptheta = np.exp(1j * theta)
+    for n in range(cutoff):
+        for k in range(cutoff):
+            for m in range(cutoff):
+                l = m - n + k
+                if 0 <= l < cutoff:
+                    gradTtheta[n, k, m, l] = 1j * (n - m) * T[n, k, m, l]
+                    gradTr[n, k, m, l] = (
+                        np.sqrt((m + 1) * (l + 1)) * T[n, k, m + 1, l + 1] * exptheta
+                    )
+                    if m > 0 and l > 0:
+                        gradTr[n, k, m, l] -= (
+                            np.sqrt(m * l) * T[n, k, m - 1, l - 1] * np.conj(exptheta)
+                        )
+
+
+def S2gate(r, theta, cutoff, grad=False, s=np.arcsinh(1.0)):
+    """Calculates the Fock representation of the S2gate and its gradient.
+
+    Arg:
+        r (float): magnitude parameter of the gate
+        theta (float): magnitude parameter of the gate
+        cutoff (int): Fock ladder cutoff
+        grad (boolean): whether to calculate the gradient or not
+        r (float): value of the parameter used internally in fock_tensor
+
+    Returns:
+        tuple[array[complex], array[complex], array[complex]]: The Fock representations of the gate and its gradients with sizes ``[cutoff]*2``
+    """
+    S = two_mode_squeezing(r, theta)
+    if not grad:
+        return fock_tensor(S, np.zeros([2]), cutoff, r=s), None, None
+
+    T = fock_tensor(S, np.zeros([2]), cutoff + 1, r=r)
+    gradTr = np.zeros([cutoff, cutoff, cutoff, cutoff], dtype=complex)
+    gradTtheta = np.zeros([cutoff, cutoff, cutoff, cutoff], dtype=complex)
+    grad_S2gate(T, gradTr, gradTtheta, theta)
+
+    return T[0:cutoff, 0:cutoff, 0:cutoff, 0:cutoff], gradTr, gradTtheta
+
+
+
+
+@jit("void(complex128[:,:,:,:],complex128[:,:,:,:], complex128[:,:,:,:], double)")
+def grad_BSgate(T, gradTr, gradTtheta, theta):  # pragma: no cover
+    """Calculates the Fock representation of the BSgate and its gradient.
+
+    Arg:
+        r (float): magnitude parameter of the gate
+        theta (float): magnitude parameter of the gate
+        cutoff (int): Fock ladder cutoff
+        grad (boolean): whether to calculate the gradient or not
+        r (float): value of the parameter used internally in fock_tensor
+
+    Returns:
+        tuple[array[complex], array[complex], array[complex]]: The Fock representations of the gate and its gradients with sizes ``[cutoff]*2``
+    """
+    cutoff = gradTr.shape[0]
+    exptheta = np.exp(1j * theta)
+
+    for n in range(cutoff):
+        for k in range(cutoff):
+            for m in range(cutoff):
+                l = n + k - m
+                if 0 <= l < cutoff:
+                    gradTtheta[n, k, m, l] = -1j * (n - m) * T[n, k, m, l]
+                    if m > 0:
+                        gradTr[n, k, m, l] = np.sqrt(m * (l + 1)) * T[n, k, m - 1, l + 1] * exptheta
+                    if l > 0:
+                        gradTr[n, k, m, l] -= (
+                            np.sqrt((m + 1) * l) * T[n, k, m + 1, l - 1] * np.conj(exptheta)
+                        )
+
+
+def BSgate(r, theta, cutoff, grad=False, s=np.arcsinh(1.0)):
+    """Calculates the Fock representation of the S2gate and its gradient.
+
+    Arg:
+        s (float): parameter of the gate
+        cutoff (int): Fock ladder cutoff
+        grad (boolean): whether to calculate the gradient or not
+        r (float): value of the parameter used internally in fock_tensor
+
+    Returns:
+        tuple[array[float], array[float] or None]: The Fock representations of the gate and its gradient with size ``[cutoff]*4``
+    """
+    S = beam_splitter(r, theta)
+    if not grad:
+        return fock_tensor(S, np.zeros([2]), cutoff, r=s), None, None
+
+    T = fock_tensor(S, np.zeros([2]), cutoff + 1, r=r)
+    gradTr = np.zeros([cutoff, cutoff, cutoff, cutoff], dtype=complex)
+    gradTtheta = np.zeros([cutoff, cutoff, cutoff, cutoff], dtype=complex)
+    grad_BSgate(T, gradTr, gradTtheta, theta)
+
+    return T[0:cutoff, 0:cutoff, 0:cutoff, 0:cutoff], gradTr, gradTtheta
+
+
+
 @jit("void(double[:,:], double[:,:], double)")
-def grad_Xgate(T, gradT, pref):# pragma: no cover
+def grad_Xgate_one_param(T, gradT, pref):# pragma: no cover
     """Calculates the gradient of the Xgate.
 
     Args:
@@ -61,7 +267,7 @@ def grad_Xgate(T, gradT, pref):# pragma: no cover
                 gradT[n, m] -= np.sqrt(m) * T[n, m - 1] * pref
 
 
-def Xgate(x, cutoff, grad=False, hbar=2, r=np.arcsinh(1.0)):
+def Xgate_one_param(x, cutoff, grad=False, hbar=2, r=np.arcsinh(1.0)):
     """Calculates the Fock representation of the Xgate and its gradient.
 
     Arg:
@@ -84,12 +290,12 @@ def Xgate(x, cutoff, grad=False, hbar=2, r=np.arcsinh(1.0)):
 
     T = fock_tensor(S, alpha, cutoff + 1, r=r)
     gradT = np.zeros([cutoff, cutoff], dtype=float)
-    grad_Xgate(T, gradT, pref)
+    grad_Xgate_one_param(T, gradT, pref)
     return T[0:cutoff, 0:cutoff], gradT
 
 
 @jit("void(complex128[:,:], complex128[:,:], double)")
-def grad_Zgate(T, gradT, pref):# pragma: no cover
+def grad_Zgate_one_param(T, gradT, pref):# pragma: no cover
     """Calculates the gradient of the Zgate.
 
     Args:
@@ -105,7 +311,7 @@ def grad_Zgate(T, gradT, pref):# pragma: no cover
                 gradT[n, m] += 1j * np.sqrt(m) * T[n, m - 1] * pref
 
 
-def Zgate(p, cutoff, grad=False, hbar=2, r=np.arcsinh(1.0)):
+def Zgate_one_param(p, cutoff, grad=False, hbar=2, r=np.arcsinh(1.0)):
     """Calculates the Fock representation of the Zgate and its gradient.
 
     Arg:
@@ -128,12 +334,12 @@ def Zgate(p, cutoff, grad=False, hbar=2, r=np.arcsinh(1.0)):
 
     T = fock_tensor(S, alpha, cutoff + 1, r=r)
     gradT = np.zeros([cutoff, cutoff], dtype=complex)
-    grad_Zgate(T, gradT, pref)
+    grad_Zgate_one_param(T, gradT, pref)
     return T[0:cutoff, 0:cutoff], gradT
 
 
 @jit("void(double[:,:], double[:,:])")
-def grad_Sgate(T, gradT):# pragma: no cover
+def grad_Sgate_one_param(T, gradT):# pragma: no cover
     """Calculates the gradient of the Sgate.
 
     Args:
@@ -149,7 +355,7 @@ def grad_Sgate(T, gradT):# pragma: no cover
                 gradT[n, m] += 0.5 * np.sqrt(m * (m - 1)) * T[n, m - 2]
 
 
-def Sgate(s, cutoff, grad=False, r=np.arcsinh(1.0)):
+def Sgate_one_param(s, cutoff, grad=False, r=np.arcsinh(1.0)):
     """Calculates the Fock representation of the Sgate and its gradient.
 
     Arg:
@@ -168,7 +374,7 @@ def Sgate(s, cutoff, grad=False, r=np.arcsinh(1.0)):
 
     T = fock_tensor(S, np.zeros([1]), cutoff + 2, r=r)
     gradT = np.zeros([cutoff, cutoff], dtype=float)
-    grad_Sgate(T, gradT)
+    grad_Sgate_one_param(T, gradT)
 
     return T[0:cutoff, 0:cutoff], gradT
 
@@ -192,7 +398,7 @@ def Rgate(theta, cutoff, grad=False):
 
 
 @jit("void(double[:,:,:,:],double[:,:,:,:])")
-def grad_S2gate(T, gradT):# pragma: no cover
+def grad_S2gate_one_param(T, gradT):# pragma: no cover
     """Calculates the gradient of the S2gate.
 
     Args:
@@ -210,7 +416,7 @@ def grad_S2gate(T, gradT):# pragma: no cover
                         gradT[n, k, m, l] -= np.sqrt(m * l) * T[n, k, m - 1, l - 1]
 
 
-def S2gate(s, cutoff, grad=False, r=np.arcsinh(1.0)):
+def S2gate_one_param(s, cutoff, grad=False, r=np.arcsinh(1.0)):
     """Calculates the Fock representation of the S2gate and its gradient.
 
     Arg:
@@ -228,13 +434,13 @@ def S2gate(s, cutoff, grad=False, r=np.arcsinh(1.0)):
 
     T = fock_tensor(S, np.zeros([2]), cutoff + 1, r=r)
     gradT = np.zeros([cutoff, cutoff, cutoff, cutoff], dtype=float)
-    grad_S2gate(T, gradT)
+    grad_S2gate_one_param(T, gradT)
 
     return T[0:cutoff, 0:cutoff, 0:cutoff, 0:cutoff], gradT
 
 
 @jit("void(double[:,:,:,:], double[:,:,:,:])")
-def grad_BSgate(T, gradT):# pragma: no cover
+def grad_BSgate_one_param(T, gradT):# pragma: no cover
     """Calculates the gradient of the BSgate.
 
     Args:
@@ -253,7 +459,7 @@ def grad_BSgate(T, gradT):# pragma: no cover
                         gradT[n, k, m, l] -= np.sqrt((m + 1) * l) * T[n, k, m + 1, l - 1]
 
 
-def BSgate(theta, cutoff, grad=False, r=np.arcsinh(1.0)):
+def BSgate_one_param(theta, cutoff, grad=False, r=np.arcsinh(1.0)):
     """Calculates the Fock representation of the BSgate and its gradient.
 
     Arg:
@@ -271,6 +477,6 @@ def BSgate(theta, cutoff, grad=False, r=np.arcsinh(1.0)):
 
     T = fock_tensor(S, np.zeros([2]), cutoff + 1, r=r)
     gradT = np.zeros([cutoff, cutoff, cutoff, cutoff], dtype=float)
-    grad_BSgate(T, gradT)
+    grad_BSgate_one_param(T, gradT)
 
     return T[0:cutoff, 0:cutoff, 0:cutoff, 0:cutoff], gradT
--- thewalrus/tests/test_fock_gradients.py
+++ thewalrus/tests/test_fock_gradients.py
@@ -13,48 +13,120 @@
 # limitations under the License.
 """Tests for The Walrus fock_gradients functions"""
 
-from thewalrus.fock_gradients import Xgate, Zgate, Sgate, S2gate, BSgate, Rgate
+from thewalrus.fock_gradients import Dgate, BSgate, Sgate, S2gate, Xgate_one_param, Zgate_one_param, Sgate_one_param, S2gate_one_param, BSgate_one_param, Rgate
 import numpy as np
 
-
-def test_Xgate():
-    """Tests the value of the analytic gradient for the Xgate against finite differences"""
-    cutoff = 10
-    x = 1.0
-    _, dX = Xgate(x, cutoff, grad=True)
-    dx = 0.001
-    Xp, _ = Xgate(x + dx, cutoff)
-    Xm, _ = Xgate(x - dx, cutoff)
-    dXfd = (Xp - Xm) / (2 * dx)
-    assert np.allclose(dX, dXfd, atol=1e-5, rtol=0)
-
-
-def test_Zgate():
-    """Tests the value of the analytic gradient for the Zgate against finite differences"""
-    cutoff = 10
-    p = 1.0
-    _, dZ = Zgate(p, cutoff, grad=True)
-    dp = 0.001
-    Zp, _ = Zgate(p + dp, cutoff)
-    Zm, _ = Zgate(p - dp, cutoff)
-    dZfd = (Zp - Zm) / (2 * dp)
-    assert np.allclose(dZ, dZfd, atol=1e-5, rtol=0)
+def test_Dgate():
+    """Tests the value of the analytic gradient for the Dgate against finite differences"""
+    cutoff = 4
+    r = 1.0
+    theta = np.pi / 8
+    _, Dr, Dtheta = Dgate(r, theta, cutoff, grad=True)
+    dr = 0.001
+    dtheta = 0.001
+    Drp, _, _ = Dgate(r + dr, theta, cutoff, grad=False)
+    Drm, _, _ = Dgate(r - dr, theta, cutoff, grad=False)
+    Dthetap, _, _ = Dgate(r, theta + dtheta, cutoff, grad=False)
+    Dthetam, _, _ = Dgate(r, theta - dtheta, cutoff, grad=False)
+    Drapprox = (Drp - Drm) / (2 * dr)
+    Dthetaapprox = (Dthetap - Dthetam) / (2 * dtheta)
+    assert np.allclose(Dr, Drapprox, atol=1e-5, rtol=0)
+    assert np.allclose(Dtheta, Dthetaapprox, atol=1e-5, rtol=0)
 
 
 def test_Sgate():
     """Tests the value of the analytic gradient for the Sgate against finite differences"""
+    cutoff = 4
+    r = 1.0
+    theta = np.pi / 8
+    _, Dr, Dtheta = Sgate(r, theta, cutoff, grad=True)
+    dr = 0.001
+    dtheta = 0.001
+    Drp, _, _ = Sgate(r + dr, theta, cutoff, grad=False)
+    Drm, _, _ = Sgate(r - dr, theta, cutoff, grad=False)
+    Dthetap, _, _ = Sgate(r, theta + dtheta, cutoff, grad=False)
+    Dthetam, _, _ = Sgate(r, theta - dtheta, cutoff, grad=False)
+    Drapprox = (Drp - Drm) / (2 * dr)
+    Dthetaapprox = (Dthetap - Dthetam) / (2 * dtheta)
+    assert np.allclose(Dr, Drapprox, atol=1e-5, rtol=0)
+    assert np.allclose(Dtheta, Dthetaapprox, atol=1e-5, rtol=0)
+
+
+def test_S2gate():
+    """Tests the value of the analytic gradient for the S2gate against finite differences"""
+    cutoff = 4
+    r = 1.0
+    theta = np.pi / 8
+    _, Dr, Dtheta = S2gate(r, theta, cutoff, grad=True)
+    dr = 0.001
+    dtheta = 0.001
+    Drp, _, _ = S2gate(r + dr, theta, cutoff, grad=False)
+    Drm, _, _ = S2gate(r - dr, theta, cutoff, grad=False)
+    Dthetap, _, _ = S2gate(r, theta + dtheta, cutoff, grad=False)
+    Dthetam, _, _ = S2gate(r, theta - dtheta, cutoff, grad=False)
+    Drapprox = (Drp - Drm) / (2 * dr)
+    Dthetaapprox = (Dthetap - Dthetam) / (2 * dtheta)
+    assert np.allclose(Dr, Drapprox, atol=1e-5, rtol=0)
+    assert np.allclose(Dtheta, Dthetaapprox, atol=1e-5, rtol=0)
+
+
+def test_BSgate():
+    """Tests the value of the analytic gradient for the BSgate against finite differences"""
+    cutoff = 4
+    r = 1.0
+    theta = np.pi / 8
+    _, Dr, Dtheta = BSgate(r, theta, cutoff, grad=True)
+    dr = 0.001
+    dtheta = 0.001
+    Drp, _, _ = BSgate(r + dr, theta, cutoff, grad=False)
+    Drm, _, _ = BSgate(r - dr, theta, cutoff, grad=False)
+    Dthetap, _, _ = BSgate(r, theta + dtheta, cutoff, grad=False)
+    Dthetam, _, _ = BSgate(r, theta - dtheta, cutoff, grad=False)
+    Drapprox = (Drp - Drm) / (2 * dr)
+    Dthetaapprox = (Dthetap - Dthetam) / (2 * dtheta)
+    assert np.allclose(Dr, Drapprox, atol=1e-4, rtol=0)
+    assert np.allclose(Dtheta, Dthetaapprox, atol=1e-4, rtol=0)
+
+
+
+def test_Xgate_one_param():
+    """Tests the value of the analytic gradient for the Xgate_one_param against finite differences"""
+    cutoff = 10
+    x = 1.0
+    _, dX = Xgate_one_param(x, cutoff, grad=True)
+    dx = 0.001
+    Xp, _ = Xgate_one_param(x + dx, cutoff)
+    Xm, _ = Xgate_one_param(x - dx, cutoff)
+    dXfd = (Xp - Xm) / (2 * dx)
+    assert np.allclose(dX, dXfd, atol=1e-5, rtol=0)
+
+
+def test_Zgate_one_param():
+    """Tests the value of the analytic gradient for the Zgate_one_param against finite differences"""
+    cutoff = 10
+    p = 1.0
+    _, dZ = Zgate_one_param(p, cutoff, grad=True)
+    dp = 0.001
+    Zp, _ = Zgate_one_param(p + dp, cutoff)
+    Zm, _ = Zgate_one_param(p - dp, cutoff)
+    dZfd = (Zp - Zm) / (2 * dp)
+    assert np.allclose(dZ, dZfd, atol=1e-5, rtol=0)
+
+
+def test_Sgate_one_param():
+    """Tests the value of the analytic gradient for the Sgate_one_param against finite differences"""
     cutoff = 10
     s = np.arcsinh(1.0)
-    _, dS = Sgate(s, cutoff, grad=True)
+    _, dS = Sgate_one_param(s, cutoff, grad=True)
     ds = 0.0001
-    Ss, _ = Sgate(s + ds, cutoff)
-    Sm, _ = Sgate(s - ds, cutoff)
+    Ss, _ = Sgate_one_param(s + ds, cutoff)
+    Sm, _ = Sgate_one_param(s - ds, cutoff)
     dSfd = (Ss - Sm) / (2 * ds)
     assert np.allclose(dS, dSfd, atol=1e-5, rtol=0)
 
 
 def test_Rgate():
-    """Tests the value of the analytic gradient for the Rgate against finite differences"""
+    """Tests the value of the analytic gradient for the Rgate_one_param against finite differences"""
     theta = 1.0
     cutoff = 9
     _, dR = Rgate(theta, cutoff, grad=True)
@@ -65,25 +137,25 @@ def test_Rgate():
     assert np.allclose(dR, dRfd, atol=1e-5, rtol=0)
 
 
-def test_S2gate():
-    """Tests the value of the analytic gradient for the S2gate against finite differences"""
+def test_S2gate_one_param():
+    """Tests the value of the analytic gradient for the S2gate_one_param against finite differences"""
     cutoff = 10
     s = np.arcsinh(1.0)
-    _, dS2 = S2gate(s, cutoff, grad=True)
+    _, dS2 = S2gate_one_param(s, cutoff, grad=True)
     ds = 0.0001
-    S2s, _ = S2gate(s + ds, cutoff)
-    S2m, _ = S2gate(s - ds, cutoff)
+    S2s, _ = S2gate_one_param(s + ds, cutoff)
+    S2m, _ = S2gate_one_param(s - ds, cutoff)
     dS2fd = (S2s - S2m) / (2 * ds)
     assert np.allclose(dS2, dS2fd, atol=1e-5, rtol=0)
 
 
-def test_BSgate():
-    """Tests the value of the analytic gradient for the BSgate against finite differences"""
+def test_BSgate_one_param():
+    """Tests the value of the analytic gradient for the BSgate_one_param against finite differences"""
     theta = 1.0
     cutoff = 9
-    _, dBS = BSgate(theta, cutoff, grad=True)
+    _, dBS = BSgate_one_param(theta, cutoff, grad=True)
     dtheta = 0.0001
-    BSs, _ = BSgate(theta + dtheta, cutoff)
-    BSm, _ = BSgate(theta - dtheta, cutoff)
+    BSs, _ = BSgate_one_param(theta + dtheta, cutoff)
+    BSm, _ = BSgate_one_param(theta - dtheta, cutoff)
     dBSfd = (BSs - BSm) / (2 * dtheta)
     assert np.allclose(dBS, dBSfd, atol=1e-5, rtol=0)
