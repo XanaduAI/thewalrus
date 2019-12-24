33cd8950d4316e84340351da7b3870e9392cd684
Nicolas Quesada <nicolas@xanadu.ai>
2019-12-24 14:05:08 -0500

    Before implementing

--- include/hermite_multidimensional.hpp
+++ include/hermite_multidimensional.hpp
@@ -158,7 +158,8 @@ inline std::vector<T> hermite_multidimensional_cpp(std::vector<T> &R_mat, std::v
     ullint Hdim = pow(resolution, dim);
     std::vector<T> H(Hdim, 0);
     std::vector<double> ren_factor(Hdim, 0);
-    std::vector<double> intsqrt(Hdim, 0);
+
+
     H[0] = 1;
     ren_factor[0] = 1;
 
@@ -251,7 +252,10 @@ inline std::vector<T> quantum_hermite_multidimensional_cpp(std::vector<T> &R_mat
 
     H[0] = 1;
     ren_factor[0] = 1;
-
+    std::vector<double> intsqrt(resolution+1, 0);
+    for (int ii = 0; ii<=resolution; ii++) {
+        intsqrt[ii] = std::sqrt((static_cast<double>(ii)))
+    }
     std::vector<int> nextPos(dim, 1);
     std::vector<int> jumpFrom(dim, 1);
     std::vector<int> ek(dim, 0);
