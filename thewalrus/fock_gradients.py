Fixes gradient of S2

thewalrus/fock_gradients.py
@@ -220,11 +220,11 @@ def grad_S2gate(T, theta, cutoff, dtype=np.complex128):  # pragma: no cover
                 l = m - n + k
                 if 0 <= l < cutoff:
                     gradTtheta[n, k, m, l] = 1j * (n - m) * T[n, k, m, l]
-                    gradTr[n, k, m, l] = -(
+                    gradTr[n, k, m, l] = (
                         np.sqrt((m + 1) * (l + 1)) * T[n, k, m + 1, l + 1] * exptheta
                     )
                     if m > 0 and l > 0:
-                        gradTr[n, k, m, l] += (
+                        gradTr[n, k, m, l] -= (
                             np.sqrt(m * l) * T[n, k, m - 1, l - 1] * np.conj(exptheta)
                         )
     return gradTr, gradTtheta