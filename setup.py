f72a4cdc5510f723fc645d180910ac2a16b33aab
Jack Brown <jack@xanadu.ai>
2021-03-04 17:51:41 -0500

    use pkg-config

--- setup.py
+++ setup.py
@@ -30,13 +30,11 @@ requirements = [
     "numba>=0.49.1",
     "dask[delayed]",
     "sympy>=1.5.1",
-    "repoze.lru>=0.7"
+    "repoze.lru>=0.7",
 ]
 
 
-setup_requirements = [
-    "numpy"
-]
+setup_requirements = ["numpy", "pkgconfig"]
 
 
 BUILD_EXT = True
@@ -46,105 +44,100 @@ try:
     from numpy.distutils.core import setup
     from numpy.distutils.extension import Extension
 except ImportError:
-    raise ImportError("ERROR: NumPy needs to be installed first. "
-                      "You can install it with pip:"
-                      "\n\npip install numpy")
+    raise ImportError(
+        "ERROR: NumPy needs to be installed first. "
+        "You can install it with pip:"
+        "\n\npip install numpy"
+    )
 
-
+EXTENSIONS = []
 if BUILD_EXT:
 
-    USE_CYTHON = True
-    try:
-        from Cython.Build import cythonize
-        ext = 'pyx'
-    except:
-        def cythonize(x, compile_time_env=None):
-            return x
+    import pkgconfig
+    from Cython.Build import cythonize
 
-        USE_CYTHON = False
-        cythonize = cythonize
-        ext = 'cpp'
+    CFLAGS = os.environ.get("CFLAGS", "-Wall -O3")
 
+    USE_OPENBLAS = bool(os.environ.get("USE_OPENBLAS"))
+    USE_LAPACK = bool(os.environ.get("USE_LAPACK")) or USE_OPENBLAS
+    USE_OPENMP = platform.system() != "Windows"
+    EIGEN_INCLUDE_DIR = os.environ.get("EIGEN_INCLUDE_DIR", "")
 
-    library_default = ""
-    USE_OPENMP = True
-    EIGEN_INCLUDE = [os.environ.get("EIGEN_INCLUDE_DIR", ""), "/usr/local/include/eigen3", "/usr/include/eigen3"]
+    CONFIG = {
+        "sources": ["thewalrus/libwalrus.pyx"],
+        "depends": [
+            "include/libwalrus.hpp",
+            "include/eigenvalue_hafnian.hpp",
+            "include/recursive_hafnian.hpp",
+            "include/repeated_hafnian.hpp",
+            "include/hafnian_approx.hpp",
+            "include/torontonian.hpp",
+            "include/permanent.hpp",
+            "include/hermite_multidimensional.hpp",
+            "include/stdafx.h",
+            "include/fsum.hpp",
+        ],
+        "extra_compile_args": [*{"-fPIC", "-std=c++14", *CFLAGS.split(" ")}],
+        "extra_link_args": [],
+        "include_dirs": ['./include'],
+        "language": "c++",
+        "libraries": ["eigen3"],
+        "library_dirs": [np.get_include()],
+    }
 
-    LD_LIBRARY_PATH = os.environ.get('LD_LIBRARY_PATH', library_default).split(":")
-    C_INCLUDE_PATH = os.environ.get('C_INCLUDE_PATH', "").split(":") + [np.get_include()]  + EIGEN_INCLUDE + ["include"]
-
-    LD_LIBRARY_PATH = [i for i in LD_LIBRARY_PATH if i]
-    libraries = []
-
-    if platform.system() == 'Windows':
-        USE_OPENMP = False
-        cflags_default = "-static -O3 -Wall -fPIC"
-        extra_link_args_CPP = ["-std=c++14 -static", "-static-libgfortran", "-static-libgcc"]
-    elif platform.system() == 'Darwin':
-        cflags_default = "-O3 -Wall -fPIC -shared -Xpreprocessor -fopenmp -lomp -mmacosx-version-min=10.9"
-        libraries += ["omp"]
-        extra_link_args_CPP = ['-Xpreprocessor -fopenmp -lomp']
-        extra_include = ['/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/']
-        C_INCLUDE_PATH += ['/usr/local/opt/libomp/include']
-        LD_LIBRARY_PATH += ['/usr/local/opt/libomp/lib']
+    if platform.system() == "Windows":
+        CONFIG["extra_compile_args"].extend(("-static",))
+        CONFIG["extra_link_args"].extend(
+            ("-static", "-static-libgfortran", "-static-libgcc")
+        )
+    elif platform.system() == "Darwin":
+        CONFIG["extra_compile_args"].extend(
+            ("-Xpreprocessor", "-fopenmp", "-mmacosx-version-min=10.9", "-shared")
+        )
+        CONFIG["extra_link_args"].extend(("-Xpreprocessor", "-fopenmp"))
+        CONFIG["library_dirs"].append(
+            "/Applications/Xcode.app/Contents/Developer/Toolchains/"
+            "XcodeDefault.xctoolchain/usr/include/c++/v1/"
+        )
     else:
-        cflags_default = "-O3 -Wall -fPIC -shared -fopenmp"
-        extra_link_args_CPP = ['-fopenmp']
+        CONFIG["extra_compile_args"].extend(("-fopenmp", "-shared"))
+        CONFIG["extra_link_args"].extend(("-fopenmp",))
 
-    CFLAGS = os.environ.get('CFLAGS', cflags_default).split() + ['-I{}'.format(np.get_include())]
+    if EIGEN_INCLUDE_DIR:
+        CONFIG["library_dirs"].append(EIGEN_INCLUDE_DIR)
 
-    USE_LAPACK = False
-    if os.environ.get("USE_LAPACK", ""):
-        USE_LAPACK = True
-        CFLAGS += [" -llapacke -DLAPACKE=1"]
-        libraries += ["lapacke"]
-        extra_link_args_CPP[0] += " -llapacke"
+    if USE_OPENBLAS:
+        CONFIG["libraries"].append("openblas")
 
-    if os.environ.get("USE_OPENBLAS", ""):
-        USE_LAPACK = True
-        CFLAGS += [" -lopenblas -DLAPACKE=1"]
-        libraries += ["openblas"]
-        extra_link_args_CPP[0] += " -lopenblas"
+    if USE_LAPACK:
+        CONFIG["libraries"].append("lapacke")
+        CONFIG["extra_compile_args"].append("-DLAPACKE=1")
 
-    extensions = cythonize([
-            Extension("libwalrus",
-                sources=["thewalrus/libwalrus."+ext,],
-                depends=["include/libwalrus.hpp",
-                         "include/eigenvalue_hafnian.hpp",
-                         "include/recursive_hafnian.hpp",
-                         "include/repeated_hafnian.hpp",
-                         "include/hafnian_approx.hpp",
-                         "include/torontonian.hpp",
-                         "include/permanent.hpp",
-                         "include/hermite_multidimensional.hpp",
-                         "include/stdafx.h",
-                         "include/fsum.hpp"],
-                include_dirs=C_INCLUDE_PATH,
-                library_dirs=['/usr/lib', '/usr/local/lib'] + LD_LIBRARY_PATH,
-                libraries=libraries,
-                language="c++",
-                extra_compile_args=["-std=c++11"] + CFLAGS,
-                extra_link_args=extra_link_args_CPP)
-    ], compile_time_env={'_OPENMP': USE_OPENMP, 'LAPACKE': USE_LAPACK})
-else:
-    extensions = []
+    for k, v in pkgconfig.parse(" ".join(CONFIG["libraries"])).items():
+        if k in CONFIG:
+            CONFIG[k].extend(v)
+        else:
+            CONFIG[k] = v
 
+    EXTENSIONS = cythonize(
+        [Extension("libwalrus", **CONFIG)],
+        compile_time_env={"_OPENMP": USE_OPENMP, "LAPACKE": USE_LAPACK})
 
 info = {
-    'name': 'thewalrus',
-    'version': version,
-    'maintainer': 'Xanadu Inc.',
-    'maintainer_email': 'nicolas@xanadu.ai',
-    'url': 'https://github.com/XanaduAI/thewalrus',
-    'license': 'Apache License 2.0',
-    'packages': find_packages(where="."),
-    'description': 'Open source library for hafnian calculation',
-    'long_description': open('README.rst').read(),
-    'provides': ["thewalrus"],
-    'install_requires': requirements,
-    'setup_requires': setup_requirements,
-    'ext_modules': extensions,
-    'ext_package': 'thewalrus'
+    "name": "thewalrus",
+    "version": version,
+    "maintainer": "Xanadu Inc.",
+    "maintainer_email": "nicolas@xanadu.ai",
+    "url": "https://github.com/XanaduAI/thewalrus",
+    "license": "Apache License 2.0",
+    "packages": find_packages(where="."),
+    "description": "Open source library for hafnian calculation",
+    "long_description": open("README.rst").read(),
+    "provides": ["thewalrus"],
+    "install_requires": requirements,
+    "setup_requires": setup_requirements,
+    "ext_modules": EXTENSIONS,
+    "ext_package": "thewalrus",
 }
 
 classifiers = [
@@ -158,12 +151,12 @@ classifiers = [
     "Operating System :: POSIX :: Linux",
     "Operating System :: Microsoft :: Windows",
     "Programming Language :: Python",
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3 :: Only',
-    "Topic :: Scientific/Engineering :: Physics"
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3 :: Only",
+    "Topic :: Scientific/Engineering :: Physics",
 ]
 
 setup(classifiers=classifiers, **(info))
