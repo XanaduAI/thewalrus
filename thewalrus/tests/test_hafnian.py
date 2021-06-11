2e84a88fd68de74d6d31b711823b0cf31629f311
ziofil <miatto@gmail.com>
2021-06-10 22:02:29 -0400

    adds tests

--- thewalrus/tests/test_hafnian.py
+++ thewalrus/tests/test_hafnian.py
@@ -278,11 +278,12 @@ class TestLoopHafnian:
         expected = np.prod(v)
         assert np.allclose(haf, expected)
 
-
     @pytest.mark.parametrize("n", [2, 3, 5, 10, 15, 20])
     @pytest.mark.parametrize("fill", [0.5, 0.2, 0.1, 0.05])
     def test_valid_output(self, random_matrix, n, fill):
         """Tests that sparse loop hafnian matches full implementation"""
         A = random_matrix(n, fill_factor=fill)
-        assert np.allclose(hafnian_sparse(A), hafnian_sparse(A, D=set(range(len(A)))))
-        assert np.allclose(hafnian(A, loop=True), hafnian_sparse(A))
+        assert np.allclose(hafnian_sparse(A, loop=True), hafnian_sparse(A, D=set(range(len(A))), loop=True))
+        assert np.allclose(hafnian_sparse(A, loop=False), hafnian_sparse(A, D=set(range(len(A))), loop=False))
+        assert np.allclose(hafnian(A, loop=True), hafnian_sparse(A, loop=True))
+        assert np.allclose(hafnian(A, loop=False), hafnian_sparse(A, loop=False))
