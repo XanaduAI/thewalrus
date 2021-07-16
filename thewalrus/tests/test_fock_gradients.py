2fab865354ee6fc78b788f03cf20e702d71a6eeb
timjank <45398597+timjank@users.noreply.github.com>
2021-07-16 09:17:16 -0400

    Adds Fock representation of MZgate (#257)
    * Corrected version
    
    * fixed error in matrix for mzgate
    
    * Added full grad_mzgate function
    
    * Added test for grad_mzgate which is passing
    
    * Adds tests, which are passing
    
    * Updates changelog
    
    * Apply suggestions from code review
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * passes black
    
    * trailing whitespace
    
    Co-authored-by: Nicolas Quesada <zeitus@gmail.com>
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- thewalrus/tests/test_fock_gradients.py
+++ thewalrus/tests/test_fock_gradients.py
@@ -21,9 +21,12 @@ from thewalrus.fock_gradients import (
     two_mode_squeezing,
     grad_two_mode_squeezing,
     beamsplitter,
-    grad_beamsplitter
+    grad_beamsplitter,
+    mzgate,
+    grad_mzgate,
 )
 import numpy as np
+import pytest
 
 
 def test_grad_displacement():
@@ -44,6 +47,7 @@ def test_grad_displacement():
     Dthetaapprox = (Dthetap - Dthetam) / (2 * dtheta)
     assert np.allclose(Dr, Drapprox, atol=1e-5, rtol=0)
     assert np.allclose(Dtheta, Dthetaapprox, atol=1e-5, rtol=0)
+
 
 def test_grad_squeezing():
     """Tests the value of the analytic gradient for the Sgate against finite differences"""
@@ -83,8 +87,9 @@ def test_grad_two_mode_squeezing():
     assert np.allclose(Dr, Drapprox, atol=1e-5, rtol=0)
     assert np.allclose(Dtheta, Dthetaapprox, atol=1e-5, rtol=0)
 
-def test_grad_beamspitter():
-    """Tests the value of the analytic gradient for the S2gate against finite differences"""
+
+def test_grad_beamsplitter():
+    """Tests the value of the analytic gradient for a beamsplitter against finite differences"""
     cutoff = 4
     r = 1.0
     theta = np.pi / 8
@@ -103,6 +108,26 @@ def test_grad_beamspitter():
     assert np.allclose(Dtheta, Dthetaapprox, atol=1e-4, rtol=0)
 
 
+def test_grad_mzgate():
+    """Tests the value of the analytic gradient for the mzgate against finite differences"""
+    cutoff = 4
+    r = 1.0
+    theta = np.pi / 8
+    T = mzgate(r, theta, cutoff)
+    Dr, Dtheta = grad_mzgate(T, r, theta)
+
+    dr = 0.001
+    dtheta = 0.001
+    Drp = mzgate(r + dr, theta, cutoff)
+    Drm = mzgate(r - dr, theta, cutoff)
+    Dthetap = mzgate(r, theta + dtheta, cutoff)
+    Dthetam = mzgate(r, theta - dtheta, cutoff)
+    Drapprox = (Drp - Drm) / (2 * dr)
+    Dthetaapprox = (Dthetap - Dthetam) / (2 * dtheta)
+    assert np.allclose(Dr, Drapprox, atol=1e-4, rtol=0)
+    assert np.allclose(Dtheta, Dthetaapprox, atol=1e-4, rtol=0)
+
+
 def test_displacement_values(tol):
     """Tests the correct construction of the single mode displacement operation"""
     cutoff = 5
@@ -111,11 +136,41 @@ def test_displacement_values(tol):
     # np.array(displace(40,alpha).data.todense())[0:5,0:5]
     expected = np.array(
         [
-            [0.84366482 + 0.00000000e00j, -0.25309944 + 4.21832408e-01j, -0.09544978 - 1.78968334e-01j, 0.06819609 + 3.44424719e-03j, -0.01109048 + 1.65323865e-02j,],
-            [0.25309944 + 4.21832408e-01j, 0.55681878 + 0.00000000e00j, -0.29708743 + 4.95145724e-01j, -0.14658716 - 2.74850926e-01j, 0.12479885 + 6.30297236e-03j,],
-            [-0.09544978 + 1.78968334e-01j, 0.29708743 + 4.95145724e-01j, 0.31873657 + 0.00000000e00j, -0.29777767 + 4.96296112e-01j, -0.18306015 - 3.43237787e-01j,],
-            [-0.06819609 + 3.44424719e-03j, -0.14658716 + 2.74850926e-01j, 0.29777767 + 4.96296112e-01j, 0.12389162 + 1.10385981e-17j, -0.27646677 + 4.60777945e-01j,],
-            [-0.01109048 - 1.65323865e-02j, -0.12479885 + 6.30297236e-03j, -0.18306015 + 3.43237787e-01j, 0.27646677 + 4.60777945e-01j, -0.03277289 + 1.88440656e-17j,],
+            [
+                0.84366482 + 0.00000000e00j,
+                -0.25309944 + 4.21832408e-01j,
+                -0.09544978 - 1.78968334e-01j,
+                0.06819609 + 3.44424719e-03j,
+                -0.01109048 + 1.65323865e-02j,
+            ],
+            [
+                0.25309944 + 4.21832408e-01j,
+                0.55681878 + 0.00000000e00j,
+                -0.29708743 + 4.95145724e-01j,
+                -0.14658716 - 2.74850926e-01j,
+                0.12479885 + 6.30297236e-03j,
+            ],
+            [
+                -0.09544978 + 1.78968334e-01j,
+                0.29708743 + 4.95145724e-01j,
+                0.31873657 + 0.00000000e00j,
+                -0.29777767 + 4.96296112e-01j,
+                -0.18306015 - 3.43237787e-01j,
+            ],
+            [
+                -0.06819609 + 3.44424719e-03j,
+                -0.14658716 + 2.74850926e-01j,
+                0.29777767 + 4.96296112e-01j,
+                0.12389162 + 1.10385981e-17j,
+                -0.27646677 + 4.60777945e-01j,
+            ],
+            [
+                -0.01109048 - 1.65323865e-02j,
+                -0.12479885 + 6.30297236e-03j,
+                -0.18306015 + 3.43237787e-01j,
+                0.27646677 + 4.60777945e-01j,
+                -0.03277289 + 1.88440656e-17j,
+            ],
         ]
     )
     T = displacement(np.abs(alpha), np.angle(alpha), cutoff)
@@ -131,26 +186,45 @@ def test_squeezing_values(tol):
     # np.array(squeeze(40,0.5*np.exp(1j*0.7)).data.todense())[0:5,0:5]
     expected = np.array(
         [
-            [0.94171062 + 0.0j, 0.0 + 0.0j, 0.23535661 - 0.19823814j, 0.0 + 0.0j, 0.02093159 - 0.12135894j,],
+            [
+                0.94171062 + 0.0j,
+                0.0 + 0.0j,
+                0.23535661 - 0.19823814j,
+                0.0 + 0.0j,
+                0.02093159 - 0.12135894j,
+            ],
             [0.0 + 0.0j, 0.83512676 + 0.0j, 0.0 + 0.0j, 0.36151137 - 0.30449682j, 0.0 + 0.0j],
-            [-0.23535661 - 0.19823814j, 0.0 + 0.0j, 0.64005396 + 0.0j, 0.0 + 0.0j, 0.42261153 - 0.35596078j,],
+            [
+                -0.23535661 - 0.19823814j,
+                0.0 + 0.0j,
+                0.64005396 + 0.0j,
+                0.0 + 0.0j,
+                0.42261153 - 0.35596078j,
+            ],
             [0.0 + 0.0j, -0.36151137 - 0.30449682j, 0.0 + 0.0j, 0.38926873 + 0.0j, 0.0 + 0.0j],
-            [0.02093159 + 0.12135894j, 0.0 + 0.0j, -0.42261153 - 0.35596078j, 0.0 + 0.0j, 0.12407853 + 0.0j,],
+            [
+                0.02093159 + 0.12135894j,
+                0.0 + 0.0j,
+                -0.42261153 - 0.35596078j,
+                0.0 + 0.0j,
+                0.12407853 + 0.0j,
+            ],
         ]
     )
     T = squeezing(r, theta, cutoff)
     assert np.allclose(T, expected, atol=tol, rtol=0)
 
 
-def test_BS_selection_rules(tol):
-    r"""Test the selection rules of a beamsplitter.
-    If one writes the beamsplitter gate of :math:`U` and its matrix elements as
+@pytest.mark.parametrize("gate", [mzgate, beamsplitter])
+def test_passive_selection_rules(tol, gate):
+    r"""Test the selection rules of a beamsplitter or a mzgate.
+    If one writes the beamsplitter or the mzgate gate as :math:`U` and its matrix elements as
     :math:`\langle m, n |U|k,l \rangle` then these elements
     are nonzero if and only if :math:`m+n = k+l`. This test checks
     that this selection rule holds.
     """
     cutoff = 4
-    T = beamsplitter(np.random.rand(), np.random.rand(), cutoff)
+    T = gate(np.random.rand(), np.random.rand(), cutoff)
     m = np.arange(cutoff).reshape(-1, 1, 1, 1)
     n = np.arange(cutoff).reshape(1, -1, 1, 1)
     k = np.arange(cutoff).reshape(1, 1, -1, 1)
@@ -172,6 +246,17 @@ def test_BS_hong_ou_mandel_interference(tol):
     cutoff = 2
     phi = 2 * np.pi * np.random.rand()
     T = beamsplitter(np.pi / 4, phi, cutoff)  # a 50-50 beamsplitter with phase phi
+    assert np.allclose(T[1, 1, 1, 1], 0.0, atol=tol, rtol=0)
+
+
+def test_MZ_hong_ou_mandel_interference(tol):
+    r"""Tests Hong-Ou-Mandel interference for a mzgate.
+    If one writes :math:`U` for the Fock representation of a mzgate
+    then it must hold that :math:`\langle 1,1|U|1,1 \rangle = 0`.
+    """
+    cutoff = 2
+    phi = 2 * np.pi * np.random.rand()
+    T = mzgate(np.pi / 2, phi, cutoff)  # a mzgate with phase phi
     assert np.allclose(T[1, 1, 1, 1], 0.0, atol=tol, rtol=0)
 
 
@@ -214,6 +299,28 @@ def test_beamsplitter_values(tol):
     U = np.array([[ct, -np.conj(st)], [st, ct]])
     # Calculate the matrix \langle i | U | j \rangle = T[i+j]
     T = beamsplitter(theta, phi, 3)
+    U_rec = np.empty([nmodes, nmodes], dtype=complex)
+    for i, vec_i in enumerate(vec_list):
+        for j, vec_j in enumerate(vec_list):
+            U_rec[i, j] = T[tuple(vec_i + vec_j)]
+    assert np.allclose(U, U_rec, atol=tol, rtol=0)
+
+
+def test_mzgate_values(tol):
+    r"""Test that the representation of an interferometer in the single
+    excitation manifold is precisely the unitary matrix that represents it
+    mode in space. This test in particular checks that the MZ gate is
+    consistent with strawberryfields
+    """
+    nmodes = 2
+    vec_list = np.identity(nmodes, dtype=int).tolist()
+    theta = 2 * np.pi * np.random.rand()
+    phi = 2 * np.pi * np.random.rand()
+    v = np.exp(1j * theta)
+    u = np.exp(1j * phi)
+    U = 0.5 * np.array([[u * (v - 1), 1j * (1 + v)], [1j * u * (1 + v), 1 - v]])
+    # Calculate the matrix \langle i | U | j \rangle = T[i+j]
+    T = mzgate(theta, phi, 3)
     U_rec = np.empty([nmodes, nmodes], dtype=complex)
     for i, vec_i in enumerate(vec_list):
         for j, vec_j in enumerate(vec_list):
