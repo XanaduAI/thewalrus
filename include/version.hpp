a9857b14192218b25932e81ef2582eb95a438b15
Theodor Isacsson <theodor@xanadu.ai>
2021-09-03 19:18:52 -0400

    bump the walrus

--- include/version.hpp
+++ include/version.hpp
@@ -21,13 +21,13 @@
 #define HAFNIAN_VERSION_MAJOR 0
 
 /// The minor version number
-#define HAFNIAN_VERSION_MINOR 16
+#define HAFNIAN_VERSION_MINOR 17
 
 /// The patch number
-#define HAFNIAN_VERSION_PATCH 1
+#define HAFNIAN_VERSION_PATCH 0
 
 /// The complete version number
 #define HAFNIAN_VERSION_CODE (HAFNIAN_VERSION_MAJOR * 10000 + HAFNIAN_VERSION_MINOR * 100 + HAFNIAN_VERSION_PATCH)
 
 /// Version number as string
-#define HAFNIAN_VERSION_STRING "0.16.1"
+#define HAFNIAN_VERSION_STRING "0.17.0"
