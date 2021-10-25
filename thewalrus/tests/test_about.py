d61bf5c0cae0f66902ac273a9cd29553ef6d81fc
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2021-10-25 14:33:13 -0500

    refactor [__init__] - unused imports

--- thewalrus/tests/test_about.py
+++ thewalrus/tests/test_about.py
@@ -18,9 +18,7 @@ Unit tests for the :mod:`thewalrus` configuration class :class:`Configuration`.
 
 import contextlib
 import io
-import importlib
 import re
-import pkg_resources
 
 import pytest
 
