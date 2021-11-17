d39a51f0bf18837646caddbf56164efe53d3bf6c
ben <lanthier_benji@hotmail.com>
2021-11-16 20:07:41 -0500

    Modified with Josh's suggestions

--- thewalrus/_permanent.py
+++ thewalrus/_permanent.py
@@ -113,7 +113,7 @@ def perm_ryser(M):
 def perm_bbfg(M):
     """
     Returns the permanent of a matrix using the bbfg formula in Gray ordering
-    
+
     The code is a re-implementation from a Python 2 code found in
     `Permanent code golf
     <https://codegolf.stackexchange.com/questions/97060/calculate-the-permanent-as-quickly-as-possible>`_
@@ -155,20 +155,20 @@ def perm_bbfg(M):
 def permanent_repeated(A, rpt):
     r"""Calculates the permanent of matrix :math:`A`, where the ith row/column
     of :math:`A` is repeated :math:`rpt_i` times.
-    
+
     This function constructs the matrix
-    
+
     .. math:: B = \begin{bmatrix} 0 & A\\ A^T & 0 \end{bmatrix},
-    
+
     and then calculates :math:`perm(A)=haf(B)`, by calling
-    
+
     >>> hafnian_repeated(B, rpt*2, loop=False)
-    
+
     Args:
         A (array): matrix of size [N, N]
         rpt (Sequence): sequence of N positive integers indicating the corresponding rows/columns
             of A to be repeated.
-            
+
     Returns:
         np.int64 or np.float64 or np.complex128: the permanent of matrix A.
     """
