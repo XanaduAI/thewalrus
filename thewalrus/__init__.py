dfabc8413adc2946d8f39ef31aea0e66b4703fca
Nicolas Quesada <991946+nquesada@users.noreply.github.com>
2021-11-25 11:53:38 -0500

    Numba permanent (#300)
    * Update _permanent.py
    
    Added Ryser algorithm to find the permanent of a matrix. Note that perm_ryser(A) can find the permanent of a real matrix as well as the permanent of a complex one.
    
    * Verification and correction of minor mistakes
    
    * Added the BBFG algorithm
    
    * Update _permanent.py
    
    Changed to possibly return ryser and bbfg instead of complex and real ryser and bbfg
    
    * Update _permanent.py
    
    Used black and pylint
    
    * Update libwalrus.pyx
    
    * Update libwalrus.pyx
    
    * Update test_permanent.py
    
    Removed imports of "perm_real", "perm_complex", "perm_BBFG_real" and "perm_BBFG_complex".
    
    * Update __init__.py
    
    * Removed the 'quad' and the 'fsum' options
    
    * Update test_permanent.py
    
    Corrected function names to be only perm.
    
    * Update libwalrus.hpp
    
    Removed #include <permanent.hpp>
    
    * Remove previous c++ permanent implementations
    
    * Remove the file permanent.hpp
    
    * Applied black and used pytest
    
    * Applied black
    
    * Put into comment all permanent
    
    * Remove permanents C++ tests
    
    * Remove C++ permanent
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * passes black
    
    * Update thewalrus/_permanent.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update thewalrus/_permanent.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update thewalrus/_permanent.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update thewalrus/_permanent.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update thewalrus/_permanent.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update thewalrus/_permanent.py
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Tried Josh's suggestions
    
    * Update _permanent.py
    
    * Apply black on the file _permanent.py
    
    * Reformat to fit in docs
    
    * Added Polyquantique team names
    
    * Modified with Josh's suggestions
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Josh Izaac <josh146@gmail.com>
    
    * Update _permanent.py
    
    * Update _permanent.py
    
    * Apply suggestions from code review
    
    * Update _permanent.py
    
    Tried to respect correct indentation
    
    * Update thewalrus/_permanent.py
    
    * Updates changelog
    
    * Fix merge conglict
    
    Co-authored-by: JQZ1111 <75086618+JQZ1111@users.noreply.github.com>
    Co-authored-by: Brandon <brandon.turcotte@polymtl.ca>
    Co-authored-by: benjaminlanthier <91567620+benjaminlanthier@users.noreply.github.com>
    Co-authored-by: Dominic <dominic.leclerc@polymtl.ca>
    Co-authored-by: ben <lanthier_benji@hotmail.com>
    Co-authored-by: Nicolas Quesada <nquesada@pop-os.localdomain>
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- thewalrus/__init__.py
+++ thewalrus/__init__.py
@@ -124,14 +124,8 @@ from ._hermite_multidimensional import (
     interferometer,
     grad_hermite_multidimensional,
 )
-from ._permanent import (
-    perm,
-    perm_complex,
-    perm_real,
-    permanent_repeated,
-    perm_BBFG_real,
-    perm_BBFG_complex,
-)
+from ._permanent import perm, permanent_repeated
+
 from ._torontonian import (
     tor,
     threshold_detection_prob_displacement,
