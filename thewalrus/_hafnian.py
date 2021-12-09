9c5ff23f9b798f43e1b0c559b219e6fa98a76ac1
JQZ1111 <75086618+JQZ1111@users.noreply.github.com>
2021-12-09 11:09:13 -0500

    Update _hafnian.py

--- thewalrus/_hafnian.py
+++ thewalrus/_hafnian.py
@@ -25,6 +25,228 @@ import numpy as np
 from j_hafnian import haf
 from j_loop_hafnian import loop_hafnian
 
+from j_loop_hafnian_subroutines import (
+    precompute_binoms,
+    matched_reps,
+    find_kept_edges,
+    f,
+    get_AX_S,
+    eigvals,
+)
+
+# pylint: disable=W0612, E1133
+@numba.jit(nopython=True, parallel=True, cache=True)
+def _calc_hafnian(A, edge_reps, glynn=True):  # pragma: no cover
+
+    r"""
+    Compute hafnian, using inputs as prepared by frontend hafnian function
+    compiled with Numba.
+    Args:
+        A (array): matrix ordered according to the chosen perfect matching.
+        edge_reps (array): how many times each edge in the perfect matching is repeated.
+        glynn (bool): whether to use finite difference sieve.
+    Returns:
+        complex128: value of hafnian.
+    """
+
+    n = A.shape[0]
+    N = 2 * edge_reps.sum()  # number of photons
+
+    if glynn:
+        steps = ((edge_reps[0] + 2) // 2) * np.prod(edge_reps[1:] + 1)
+    else:
+        steps = np.prod(edge_reps + 1)
+
+    # precompute binomial coefficients
+    max_binom = edge_reps.max() + 1
+    binoms = precompute_binoms(max_binom)
+
+    H = np.complex128(0)  # start running total for the hafnian
+
+    for j in numba.prange(steps):
+
+        kept_edges = find_kept_edges(j, edge_reps)
+        edge_sum = kept_edges.sum()
+
+        binom_prod = 1.0
+        for i in range(n // 2):
+            binom_prod *= binoms[edge_reps[i], kept_edges[i]]
+
+        if glynn:
+            kept_edges = 2 * kept_edges - edge_reps
+
+        AX_S = get_AX_S(kept_edges, A)
+
+        E = eigvals(AX_S)  # O(n^3) step
+
+        prefac = (-1.0) ** (N // 2 - edge_sum) * binom_prod
+
+        if glynn and kept_edges[0] == 0:
+            prefac *= 0.5
+        Hnew = prefac * f(E, N)[N // 2]
+
+        H += Hnew
+
+    if glynn:
+        H = H * 0.5 ** (N // 2 - 1)
+
+    return H
+
+def haf(A, reps=None, glynn=True):
+    r"""
+    Calculate hafnian with (optional) repeated rows and columns.
+    Args:
+        A (array): N x N matrix.
+        reps (list): length-N list of repetitions of each row/col (optional), if not provided, each row/column
+                    assumed to be repeated once.
+        glynn (bool): If True, use Glynn-style finite difference sieve formula, if False, use Ryser style inclusion/exclusion principle.
+    Returns
+        np.complex128: result of hafnian calculation.
+    """
+
+    n = A.shape[0]
+
+    if reps is None:
+        reps = [1] * n
+
+    N = sum(reps)
+
+    if N == 0:
+        return 1.0
+
+    if N % 2 == 1:
+        return 0.0
+
+    assert n == len(reps)
+
+    x, edge_reps, oddmode = matched_reps(reps)
+
+    # make new A matrix using the ordering from above
+
+    Ax = A[np.ix_(x, x)].astype(np.complex128)
+
+    H = _calc_hafnian(Ax, edge_reps, glynn)
+    return H
+
+# pylint: disable=too-many-arguments, redefined-outer-name, not-an-iterable
+@numba.jit(nopython=True, parallel=True, cache=True)
+def _calc_loop_hafnian(
+    A, D, edge_reps, oddloop=None, oddV=None, glynn=True
+):  # pragma: no cover
+    """
+    Compute loop hafnian, using inputs as prepared by frontend loop_hafnian function
+    compiled with Numba.
+    Args:
+        A (array): Matrix ordered according to the chosen perfect matching.
+        D (array): Diagonals ordered according to the chosen perfect matching.
+        edge_reps (array): How many times each edge in the perfect matching is repeated.
+        oddloop (float): Weight of self-loop in perfect matching, None if no self-loops.
+        oddV (array): Row of matrix corresponding to the odd loop in the perfect matching.
+        glynn (bool): Whether to use finite difference sieve.
+    Returns:
+        complex128: Value of loop hafnian.
+    """
+
+    n = A.shape[0]
+    N = 2 * edge_reps.sum()  # Number of photons
+    if oddloop is not None:
+        N += 1
+    if glynn and (oddloop is None):
+        steps = ((edge_reps[0] + 2) // 2) * np.prod(edge_reps[1:] + 1)
+    else:
+        steps = np.prod(edge_reps + 1)
+
+    # Precompute binomial coefficients
+    max_binom = edge_reps.max() + 1
+    binoms = precompute_binoms(max_binom)
+
+    H = np.complex128(0)  # Start running total for the hafnian
+
+    for j in numba.prange(steps):
+
+        kept_edges = find_kept_edges(j, edge_reps)
+        edge_sum = kept_edges.sum()
+
+        binom_prod = 1.0
+        for i in range(n // 2):
+            binom_prod *= binoms[edge_reps[i], kept_edges[i]]
+
+        if glynn:
+            kept_edges = 2 * kept_edges - edge_reps
+
+        AX_S, XD_S, D_S, oddVX_S = get_submatrices(kept_edges, A, D, oddV)
+
+        E = eigvals(AX_S)  # O(n^3) step
+
+        prefac = (-1.0) ** (N // 2 - edge_sum) * binom_prod
+
+        if oddloop is not None:
+            Hnew = prefac * f_loop_odd(E, AX_S, XD_S, D_S, N, oddloop, oddVX_S)[N]
+        else:
+            if glynn and kept_edges[0] == 0:
+                prefac *= 0.5
+            Hnew = prefac * f_loop(E, AX_S, XD_S, D_S, N)[N // 2]
+
+        H += Hnew
+
+    if glynn:
+        if oddloop is None:
+            H = H * 0.5 ** (N // 2 - 1)
+        else:
+            H = H * 0.5 ** (N // 2)
+
+    return H
+
+# pylint: disable=redefined-outer-name
+def loop_hafnian(A, D=None, reps=None, glynn=True):
+    """
+    Calculate loop hafnian with (optional) repeated rows and columns.
+    Args:
+        A (array): N x N matrix.
+        D (array): Diagonal entries of matrix (optional). If not provided, D is the diagonal of A.
+                    If repetitions are provided, D should be provided explicitly.
+        reps (list): Length-N list of repetitions of each row/col (optional), if not provided, each row/column
+                    assumed to be repeated once.
+        glynn (bool): If True, use Glynn-style finite difference sieve formula, if False, use Ryser style inclusion/exclusion principle.
+    Returns
+        np.complex128: Result of loop hafnian calculation.
+    """
+
+    n = A.shape[0]
+
+    if reps is None:
+        reps = [1] * n
+    if D is None:
+        D = A.diagonal()
+
+    N = sum(reps)
+
+    if N == 0:
+        return 1.0
+
+    if N == 1:
+        return D[0]
+
+    assert n == len(reps)
+
+    assert D.shape[0] == n
+
+    x, edge_reps, oddmode = matched_reps(reps)
+
+    # Make new A matrix and D vector using the ordering from above
+
+    if oddmode is not None:
+        oddloop = D[oddmode].astype(np.complex128)
+        oddV = A[oddmode, x].astype(np.complex128)
+    else:
+        oddloop = None
+        oddV = None
+
+    Ax = A[np.ix_(x, x)].astype(np.complex128)
+    Dx = D[x].astype(np.complex128)
+
+    H = _calc_loop_hafnian(Ax, Dx, edge_reps, oddloop, oddV, glynn)
+    return H
 
 def input_validation(A, rtol=1e-05, atol=1e-08):
     """Checks that the matrix A satisfies the requirements for Hafnian calculation.
