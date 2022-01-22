e5ba0632f1ab746b37834879b9765870b77fa9c7
JQZ1111 <75086618+JQZ1111@users.noreply.github.com>
2022-01-22 18:27:15 -0500

    Modified so numba should be happy

--- thewalrus/recursive_hafnian.py
+++ thewalrus/recursive_hafnian.py
@@ -5,26 +5,34 @@ import numba
 
 @numba.jit
 def hafnian(m):
-  n = len(m)/2
-  z = [[0]*(n+1) for _ in range(n*(2*n-1))]
-  for j in range(1, 2*n):
-    for k in range(j):
-      z[j*(j-1)/2+k][0] = m[j][k]
-  return solve(z, 2*n, 1, [1] + [0]*n, n)
+    n = int(float(len(m))/2)
+    z = np.zeros((n*(2*n-1),n+1))
+    for j in range(1, 2*n):
+        for k in range(j):
+            z[int(j*(j-1)/2+k)][0] = m.copy()[j][k]
+    g = np.zeros(n+1)
+    g[0] = 1
+    return solve(z, 2*n, 1, g, n)
 
 @numba.jit
-def solve(b, s, w, g, n):
-  if s == 0:
-    return w*g[n]
-  c = [b[(j+1)*(j+2)/2+k+2][:] for j in range(1, s-2) for k in range(j)]
-  h = solve(c, s-2, -w, g, n)
-  e = g[:]
-  for u in range(n):
-    for v in range(n-u):
-      e[u+v+1] += g[u]*b[0][v]
-  for j in range(1, s-2):
-    for k in range(j):
-      for u in range(n):
+def solve(b,s,w,g,n):
+    if s == 0:
+        return w*g[n]
+    c = np.zeros((int((s-2)*(s-3)/2), n+1))
+    i=0
+    for j in range(1,s-2):
+        for k in range(j):
+            c[i] = b[int((j+1)*(j+2)/2+k+2)]
+            i+=1
+    h = solve(c, s-2, -w, g, n)
+    e = g[:].copy()
+    for u in range(n):
         for v in range(n-u):
-          c[j*(j-1)/2+k][u+v+1] += b[(j+1)*(j+2)/2][u]*b[(k+1)*(k+2)/2+1][v] + b[(k+1)*(k+2)/2][u]*b[(j+1)*(j+2)/2+1][v]
-  return h + solve(c, s-2, w, e, n)
+            e[u+v+1] += g[u]*b[0][v]
+    for j in range(1, s-2):
+        for k in range(j):
+            for u in range(n):
+                for v in range(n-u):
+                    c[int(j*(j-1)/2+k)][u+v+1] += b[int((j+1)*(j+2)/2)][u]*b[int((k+1)*(k+2)/2+1)][v] + b[int((k+1)*(k+2)/2)][u]*b[int((j+1)*(j+2)/2+1)][v]
+    print(c,g)
+    return h + solve(c, s-2, w, e, n)
