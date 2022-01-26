dfdd230e0c38499fbf6a7845231e5e42a04af6c4
fab1an-q <fabian@xanadu.ai>
2022-01-26 15:27:37 -0500

    tests

--- thewalrus/quantum/entanglement.py
+++ thewalrus/quantum/entanglement.py
@@ -1,15 +1,33 @@
+# Copyright 2019-2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Entanglement measures for pure and mixed Gaussian states.
+"""
+
 import numpy as np
 from ..decompositions import symplectic_eigenvals
 from .gaussian_checks import is_valid_cov, is_pure_cov
 from .conversions import reduced_gaussian
 
-def get_partition(modes_A, separation, M):
+
+def get_partition(modes_A, split, M):
     r"""Helper function to validate and, if necessary, pre-prepare the inputs
     of ``vonNeumann_entropy()`` and ``log_negativity()``.
 
     Args:
         modes_A (iterable or int): the subset of modes used for the bipartition
-        separation (int): the index of the mode separating the two partitions
+        split (int): the index of the mode separating the two partitions
             (alternative to ``modes_A``)
         M (int): total number of modes
 
@@ -18,19 +36,31 @@ def get_partition(modes_A, separation, M):
     """
     if modes_A is not None:
         if not isinstance(modes_A, (int, range, list, tuple, np.ndarray)):
-            raise TypeError("modes_A must be either integer, range, tuple or np.ndarray.")
+            raise TypeError(
+                "modes_A must be either integer, range, tuple or np.ndarray."
+            )
         if isinstance(modes_A, int):
-            modes_A = modes_A
+            modes_A = [modes_A]
         for mode in modes_A:
             if not isinstance(mode, int) or mode < 0 or mode > M - 1:
-                raise ValueError(f"Every element of modes_A must be an integer between 0 and {M - 1}")
+                raise ValueError(
+                    f"Every element of modes_A must be an integer between 0 and {M - 1}"
+                )
 
-    if modes_A is None and separation is not None:
-        if not isinstance(separation, int) or separation > M - 1:
-            raise ValueError("separation must be an integer smaller than the number of modes.")
-        modes_A = range(separation)
+    if modes_A is None and split is not None:
+        if not isinstance(split, int) or split > M - 1:
+            raise ValueError(
+                "split must be an integer smaller than the number of modes."
+            )
+        modes_A = range(split)
+
+    if modes_A is None and split is None:
+        raise TypeError(
+            "A bipartition must be defined, either by ``modes_A`` or ``split``."
+        )
 
     return list(modes_A)
+
 
 def vonNeumann_entropy(cov):
     r"""Returns the vonNeumann entropy of a covariance matrix.
@@ -49,19 +79,22 @@ def vonNeumann_entropy(cov):
     S = 0
     for nu in nus:
         if not np.isclose(nu, 1):
-            g = (nu + 1) / 2 * np.log((nu + 1) / 2) - (nu - 1) / 2 * np.log((nu - 1) / 2)
+            g = (nu + 1) / 2 * np.log((nu + 1) / 2) - (nu - 1) / 2 * np.log(
+                (nu - 1) / 2
+            )
             S += g
 
     return S
 
-def entanglement_entropy(cov, modes_A=None, separation=None):
+
+def entanglement_entropy(cov, modes_A=None, split=None):
     r"""Returns the entanglement entropy of a covariance matrix under a given
     bipartition.
 
     Args:
         cov (array): a covariance matrix
         modes_A (iterable or int): the subset of modes used for the bipartition
-        separation (int): the index of the mode separating the two partitions
+        split (int): the index of the mode separating the two partitions
             (alternative to ``modes_A``)
 
     Returns:
@@ -72,7 +105,7 @@ def entanglement_entropy(cov, modes_A=None, separation=None):
 
     M = int(len(cov) / 2)
 
-    modes_A = get_partition(modes_A, separation, M)
+    modes_A = get_partition(modes_A, split, M)
 
     _, cov_A = reduced_gaussian(np.zeros(2 * M), cov, modes_A)
 
@@ -80,14 +113,15 @@ def entanglement_entropy(cov, modes_A=None, separation=None):
 
     return E
 
-def log_negativity(cov, modes_A=None, separation=None):
+
+def log_negativity(cov, modes_A=None, split=None):
     r"""Returns the logarithmic negativity of a covariance matrix under a given
     bipartition.
 
     Args:
         cov (array): a covariance matrix
         modes_A (iterable or int): the subset of modes used for the bipartition
-        separation (int): the index of the mode separating the two partitions
+        split (int): the index of the mode separating the two partitions
             (alternative to ``modes_A``)
 
     Returns:
@@ -98,7 +132,7 @@ def log_negativity(cov, modes_A=None, separation=None):
 
     M = int(len(cov) / 2)
 
-    modes_A = get_partition(modes_A, separation, M)
+    modes_A = get_partition(modes_A, split, M)
 
     X = np.ones(M)
     P = np.ones(M)
---
+++ thewalrus/tests/test_entanglement.py
@@ -0,0 +1,128 @@
+# Copyright 2019 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Entanglement tests"""
+# pylint: disable=no-self-use, assignment-from-no-return
+import pytest
+
+import numpy as np
+
+from thewalrus.quantum import entanglement_entropy, log_negativity
+from thewalrus.random import random_interferometer
+from thewalrus.symplectic import interferometer, passive_transformation
+
+
+def random_cov(num_modes=200, pure=False, nonclassical=True):
+    r"""Creates a random covariance matrix for testing.
+
+    Args:
+        num_modes (int): number of modes
+        pure (bool): Gaussian state is pure
+        nonclassical (bool): Gaussian state is nonclassical
+
+    Returns:
+        (array): a covariance matrix
+    """
+    M = num_modes
+    O = interferometer(random_interferometer(M))
+
+    if pure:
+        eta = 1
+    elif not pure:
+        eta = 0.123
+
+    r = 1.234
+    if nonclassical:
+        # squeezed inputs
+        cov_in = np.diag(
+            np.concatenate([np.exp(2 * r) * np.ones(M), np.exp(-2 * r) * np.ones(M)])
+        )
+    elif not nonclassical and pure:
+        # vacuum inputs
+        cov_in = np.eye(2 * M)
+    elif not nonclassical and not pure:
+        # squashed inputs
+        cov_in = np.diag(np.concatenate([np.exp(2 * r) * np.ones(M), np.ones(M)]))
+
+    cov_out = O @ cov_in @ O.T
+    _, cov = passive_transformation(
+        np.zeros([len(cov_out)]), cov_out, np.sqrt(eta) * np.identity(len(cov_out) // 2)
+    )
+
+    return cov
+
+
+def test_entanglement_entropy_bipartition():
+    """Tests if both substates of a bipartition will yield the same
+    entanglement entropy"""
+    M = 200
+    cov = random_cov(num_modes=M, pure=True, nonclassical=True)
+    modes_A = [1, 11, 111]
+    # make a list that contains all modes but 1, 11, and 111:
+    modes_B = list(range(M))
+    modes_B.pop(111)
+    modes_B.pop(11)
+    modes_B.pop(1)
+    E_A = entanglement_entropy(cov, modes_A=modes_A)
+    E_B = entanglement_entropy(cov, modes_A=modes_B)
+    assert np.isclose(E_A, E_B)
+
+
+def test_log_negativity_bipartition():
+    """Tests if both substates of a bipartition will yield the same log
+    negativity"""
+    M = 200
+    cov = random_cov(num_modes=M, pure=False, nonclassical=True)
+    modes_A = [1, 11, 111]
+    # make a list that contains all modes but 1, 11, and 111:
+    modes_B = list(range(M))
+    modes_B.pop(111)
+    modes_B.pop(11)
+    modes_B.pop(1)
+    E_A = log_negativity(cov, modes_A=modes_A)
+    E_B = log_negativity(cov, modes_A=modes_B)
+    assert np.isclose(E_A, E_B)
+
+
+def test_entanglement_entropy_classical():
+    """Tests if a classical state will return entanglement entropy zero"""
+    cov = random_cov(pure=True, nonclassical=False)
+    E = entanglement_entropy(cov, modes_A=[10, 20, 30, 40])
+    assert np.isclose(E, 0)
+
+
+def test_log_negativity_classical():
+    """Tests if a classical state will return log negativity zero"""
+    cov = random_cov(pure=False, nonclassical=False)
+    E = log_negativity(cov, modes_A=[10, 20, 30, 40])
+    assert np.isclose(E, 0)
+
+
+def test_entanglement_entropy_split():
+    """Tests the equivalence of the two alternative inputs ``modes_A`` and
+    ``split``"""
+    cov = random_cov(pure=True, nonclassical=True)
+    split = 123
+    E_0 = entanglement_entropy(cov, split=split)
+    E_1 = entanglement_entropy(cov, modes_A=range(split))
+    assert np.isclose(E_0, E_1)
+
+
+def test_log_negativity_split():
+    """Tests the equivalence of the two alternative inputs ``modes_A`` and
+    ``split``"""
+    cov = random_cov(pure=False, nonclassical=True)
+    split = 123
+    E_0 = log_negativity(cov, split=split)
+    E_1 = log_negativity(cov, modes_A=range(split))
+    assert np.isclose(E_0, E_1)
