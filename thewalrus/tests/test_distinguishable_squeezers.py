9243471f642ed89767024aa7b30db0f6ea7c2cec
Nicolas Quesada <nquesada@pop-os.localdomain>
2022-02-08 16:40:35 -0500

    Fixes minor typo

--- thewalrus/tests/test_distinguishable_squeezers.py
+++ thewalrus/tests/test_distinguishable_squeezers.py
@@ -28,7 +28,7 @@ def test_moments_of_distinguishable_distribution(M):
     T = (unitary_group.rvs(M) * np.random.rand(M)) @ unitary_group.rvs(M)
     rs = np.random.rand(M)
     num_samples = 100000
-    samples = sample(rs, T, n_samples=num_samples)
+    samples = sample(T, rs, n_samples=num_samples)
     means = samples.mean(axis=0)
     cov = np.cov(samples.T)
     expected_means = number_means(T, rs)
