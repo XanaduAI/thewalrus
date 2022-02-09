db207d553d279409fd09385decbaa23311d215d6
JQZ1111 <75086618+JQZ1111@users.noreply.github.com>
2022-02-08 22:18:47 -0500

    Added error if recursive and loop

--- thewalrus/_hafnian.py
+++ thewalrus/_hafnian.py
@@ -774,9 +774,9 @@ def hafnian(
 
     if recursive:
         if loop:
-            warnings.warn("Recursive algorithm does not support the loop hafnian")
+            raise TypeError("Recursive algorithm does not support the loop hafnian")
 
-            return recursive_hafnian(A)
+        return recursive_hafnian(A)
 
     return _haf(A, reps=None, glynn=glynn)
 
