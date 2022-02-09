aa5291ad68de893cb1dd923282712b6bbf7972b9
JQZ1111 <75086618+JQZ1111@users.noreply.github.com>
2022-02-09 10:32:03 -0500

    Corrected some errors

--- thewalrus/tests/test_hafnian_recursive.py
+++ thewalrus/tests/test_hafnian_recursive.py
@@ -34,6 +34,6 @@ def test_recursive_or_loop():
     """Check exception raised if chosen loop and recursive"""
     loop = True
     recursive = True
-    A = np.random.rand((3,3))
+    A = np.random.rand(3,3)
     with pytest.raises(TypeError, match="Recursive algorithm cannot support loop"):
         hafnian(A, recursive = recursive, loop = loop)
