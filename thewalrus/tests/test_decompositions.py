6c4de003d1a70e74bace32eb80f7bc85e4e3c22a
Nicolas Quesada <991946+nquesada@users.noreply.github.com>
2023-05-09 16:35:11 -0400

    Update decompositions.py and readds the Takagi decomposition (#363)
    * Update decompositions.py
    
    * New takagi
    
    * New takagi
    
    * Passes black
    
    * Simplifies blochmessiah
    
    * Simplifies blochmessiah
    
    * Found a case that breaks Takagi
    
    * Fixes all the tests
    
    * Fixes issues found by the linter
    
    * Adds extra test
    
    * Adds extra test
    
    * dummy
    
    * dummy
    
    * fix [.readthedocs.yml] - increase python version
    
    Fixes urllib3 v2.0 not supporting versions <1.1.1 of openSSL. See https://github.com/readthedocs/readthedocs.org/issues/10290.
    
    * fix [.readthedocs.yml] - set container image and python version
    
    * unpin doc requirements
    
    * Revert "fix [.readthedocs.yml] - increase python version"
    
    This reverts commit db0e8d7c6fc6838e0aa2126b3ae813fc2397fcba.
    
    ---------
    
    Co-authored-by: Nicolas Quesada <nquesada@pop-os.localdomain>
    Co-authored-by: Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>

--- thewalrus/tests/test_decompositions.py
+++ thewalrus/tests/test_decompositions.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Xanadu Quantum Technologies Inc.
+# Copyright 2023 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ from scipy.linalg import block_diag
 
 from thewalrus.random import random_interferometer as haar_measure
 from thewalrus.random import random_symplectic
-from thewalrus.decompositions import williamson, blochmessiah
+from thewalrus.decompositions import williamson, blochmessiah, takagi
 from thewalrus.symplectic import sympmat as omega
 from thewalrus.quantum.gaussian_checks import is_symplectic
 
@@ -253,3 +253,144 @@ class TestBlochMessiahDecomposition:
         assert np.allclose(O1.T @ O @ O1, O, atol=tol, rtol=0)
         assert np.allclose(O2.T @ O @ O2, O, atol=tol, rtol=0)
         assert np.allclose(S @ O @ S.T, O, atol=tol, rtol=0)
+
+
+@pytest.mark.parametrize("n", [5, 10, 50])
+@pytest.mark.parametrize("datatype", [np.complex128, np.float64])
+@pytest.mark.parametrize("svd_order", [True, False])
+def test_takagi(n, datatype, svd_order):
+    """Checks the correctness of the Takagi decomposition function"""
+    if datatype is np.complex128:
+        A = np.random.rand(n, n) + 1j * np.random.rand(n, n)
+    if datatype is np.float64:
+        A = np.random.rand(n, n)
+    A += A.T
+    r, U = takagi(A, svd_order=svd_order)
+    assert np.allclose(A, U @ np.diag(r) @ U.T)
+    assert np.all(r >= 0)
+    if svd_order is True:
+        assert np.all(np.diff(r) <= 0)
+    else:
+        assert np.all(np.diff(r) >= 0)
+
+
+@pytest.mark.parametrize("n", [5, 10, 50])
+@pytest.mark.parametrize("datatype", [np.complex128, np.float64])
+@pytest.mark.parametrize("svd_order", [True, False])
+@pytest.mark.parametrize("half_rank", [0, 1])
+@pytest.mark.parametrize("phase", [0, 1])
+def test_degenerate(n, datatype, svd_order, half_rank, phase):
+    """Tests Takagi produces the correct result for very degenerate cases"""
+    nhalf = n // 2
+    diags = [half_rank * np.random.rand()] * nhalf + [np.random.rand()] * (n - nhalf)
+    if datatype is np.complex128:
+        U = haar_measure(n)
+    if datatype is np.float64:
+        U = np.exp(1j * phase) * haar_measure(n, real=True)
+    A = U @ np.diag(diags) @ U.T
+    r, U = takagi(A, svd_order=svd_order)
+    assert np.allclose(A, U @ np.diag(r) @ U.T)
+    assert np.allclose(U @ U.T.conj(), np.eye(n))
+    assert np.all(r >= 0)
+    if svd_order is True:
+        assert np.all(np.diff(r) <= 0)
+    else:
+        assert np.all(np.diff(r) >= 0)
+
+
+def test_zeros():
+    """Verify that the Takagi decomposition returns a zero vector and identity matrix when
+    input a matrix of zeros"""
+    dim = 4
+    a = np.zeros((dim, dim))
+    rl, U = takagi(a)
+    assert np.allclose(rl, np.zeros(dim))
+    assert np.allclose(U, np.eye(dim))
+
+
+def test_takagi_error():
+    """Tests the value errors of Takagi"""
+    n = 10
+    m = 11
+    A = np.random.rand(n, m)
+    with pytest.raises(ValueError, match="The input matrix is not square"):
+        takagi(A)
+
+
+def test_real_degenerate():
+    """Verify that the Takagi decomposition returns a matrix that is unitary and results in a
+    correct decomposition when input a real but highly degenerate matrix. This test uses the
+    adjacency matrix of a balanced tree graph."""
+
+    vals = [
+        1,
+        2,
+        31,
+        34,
+        35,
+        62,
+        67,
+        68,
+        94,
+        100,
+        101,
+        125,
+        133,
+        134,
+        157,
+        166,
+        167,
+        188,
+        199,
+        200,
+        220,
+        232,
+        233,
+        251,
+        265,
+        266,
+        283,
+        298,
+        299,
+        314,
+        331,
+        332,
+        346,
+        364,
+        365,
+        377,
+        397,
+        398,
+        409,
+        430,
+        431,
+        440,
+        463,
+        464,
+        472,
+        503,
+        535,
+        566,
+        598,
+        629,
+        661,
+        692,
+        724,
+        755,
+        787,
+        818,
+        850,
+        881,
+        913,
+        944,
+    ]
+    mat = np.zeros([31 * 31])
+    mat[vals] = 1
+    mat = mat.reshape(31, 31)
+    # The lines above are equivalent to:
+    # import networkx as nx
+    # g = nx.balanced_tree(2, 4)
+    # a = nx.to_numpy_array(g)
+    rl, U = takagi(mat)
+    assert np.allclose(U @ U.conj().T, np.eye(len(mat)))
+    assert np.allclose(U @ np.diag(rl) @ U.T, mat)
