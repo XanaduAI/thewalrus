c625410f1ee3a0248736437d3017783c3f20ab57
Nicolas Quesada <991946+nquesada@users.noreply.github.com>
2023-05-30 20:41:22 -0400

    Merging master into internal_modes (#365)
    * Refactor sphinx config and bump docs python version (#364)
    
    * fix [.readthedocs.yml] - increase python version
    
    Fixes urllib3 v2.0 not supporting versions <1.1.1 of openSSL. See https://github.com/readthedocs/readthedocs.org/issues/10290.
    
    * fix [.readthedocs.yml] - set container image and python version
    
    * unpin doc requirements
    
    * Update decompositions.py and readds the Takagi decomposition (#363)
    
    * Update decompositions.py
    
    * New takagi
    
    * New takagi
    
    * Passes black
    
    * Simplifies blochmessiah
    
    * Simplifies blochmessiah
    
    * Found a case that breaks Takagi
    
    * Fixes all the tests
    
    * Fixes issues found by the linter
    
    * Adds extra test
    
    * Adds extra test
    
    * dummy
    
    * dummy
    
    * fix [.readthedocs.yml] - increase python version
    
    Fixes urllib3 v2.0 not supporting versions <1.1.1 of openSSL. See https://github.com/readthedocs/readthedocs.org/issues/10290.
    
    * fix [.readthedocs.yml] - set container image and python version
    
    * unpin doc requirements
    
    * Revert "fix [.readthedocs.yml] - increase python version"
    
    This reverts commit db0e8d7c6fc6838e0aa2126b3ae813fc2397fcba.
    
    ---------
    
    Co-authored-by: Nicolas Quesada <nquesada@pop-os.localdomain>
    Co-authored-by: Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
    
    * Dummy commit
    
    * Passes black
    
    * linting
    
    * Updates module summary
    
    ---------
    
    Co-authored-by: Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
    Co-authored-by: Nicolas Quesada <nquesada@pop-os.localdomain>

--- thewalrus/decompositions.py
+++ thewalrus/decompositions.py
@@ -26,7 +26,6 @@ Summary
 -------
 
 .. autosummary::
-    autonne
     blochmessiah
     symplectic_eigenvals
     takagi
@@ -35,209 +34,11 @@ Summary
 Code details
 ------------
 """
-from itertools import groupby
 import numpy as np
 
 from scipy.linalg import block_diag, sqrtm, schur
 from thewalrus.symplectic import sympmat
 from thewalrus.quantum.gaussian_checks import is_symplectic
-
-
-def autonne(A, rtol=1e-05, atol=1e-08, svd_order=True):
-    r"""Autonne-Takagi decomposition of a complex symmetric (not Hermitian!) matrix.
-
-    Args:
-        A (array): square, symmetric matrix
-        rtol (float): the relative tolerance parameter between ``A`` and ``A.T``
-        atol (float): the absolute tolerance parameter between ``A`` and ``A.T``
-        svd_order (boolean): whether to return result by ordering the singular values of ``A`` in descending (``True``) or asceding (``False``) order.
-
-    Returns:
-        tuple[array, array]: (r, U), where r are the singular values,
-        and U is the Autonne-Takagi unitary, such that :math:`A = U \diag(r) U^T`.
-    """
-    n, m = A.shape
-    if n != m:
-        raise ValueError("The input matrix is not square")
-    if not np.allclose(A, A.T, rtol=rtol, atol=atol):
-        raise ValueError("The input matrix is not symmetric")
-    Areal = A.real
-    Aimag = A.imag
-
-    B = np.empty((2 * n, 2 * n))
-    B[:n, :n] = Areal
-    B[n : 2 * n, :n] = Aimag
-    B[:n, n : 2 * n] = Aimag
-    B[n : 2 * n, n : 2 * n] = -Areal
-    vals, vects = np.linalg.eigh(B)
-    U = vects[:n, n : 2 * n] + 1j * vects[n : 2 * n, n : 2 * n]
-    if svd_order:
-        return (vals[n : 2 * n])[::-1], U[:, ::-1]
-    return vals[n : 2 * n], U
-
-
-def blochmessiah(S):
-    """Returns the Bloch-Messiah decomposition of a symplectic matrix S = uff @ dff @ vff
-       where uff and vff are orthogonal symplectic matrices and dff is a diagonal matrix
-       of the form diag(d1,d2,...,dn,d1^-1, d2^-1,...,dn^-1),
-
-    Args:
-        S (array[float]): 2N x 2N real symplectic matrix
-
-    Returns:
-        tupple(array[float],  : orthogonal symplectic matrix uff
-               array[float],  : diagional matrix dff
-               array[float])  : orthogonal symplectic matrix vff
-    """
-
-    N, _ = S.shape
-
-    if not is_symplectic(S):
-        raise ValueError("Input matrix is not symplectic.")
-
-    # Changing Basis
-    R = (1 / np.sqrt(2)) * np.block(
-        [[np.eye(N // 2), 1j * np.eye(N // 2)], [np.eye(N // 2), -1j * np.eye(N // 2)]]
-    )
-    Sc = R @ S @ R.conj().T
-    # Polar Decomposition
-    u1, d1, v1 = np.linalg.svd(Sc)
-    Sig = u1 @ np.diag(d1) @ u1.conj().T
-    Unitary = u1 @ v1
-    # Blocks of Unitary and Hermitian symplectics
-    alpha = Unitary[0 : N // 2, 0 : N // 2]
-    beta = Sig[0 : N // 2, N // 2 : N]
-    # Bloch-Messiah in this Basis
-    u2, d2, v2 = np.linalg.svd(beta)
-    sval = np.arcsinh(d2)
-    takagibeta = u2 @ sqrtm(u2.conj().T @ (v2.T))
-    uf = np.block([[takagibeta, 0 * takagibeta], [0 * takagibeta, takagibeta.conj()]])
-    vf = np.block(
-        [
-            [takagibeta.conj().T @ alpha, 0 * takagibeta],
-            [0 * takagibeta, (takagibeta.conj().T @ alpha).conj()],
-        ]
-    )
-    df = np.block(
-        [
-            [np.diag(np.cosh(sval)), np.diag(np.sinh(sval))],
-            [np.diag(np.sinh(sval)), np.diag(np.cosh(sval))],
-        ]
-    )
-    # Rotating Back to Original Basis
-    uff = R.conj().T @ uf @ R
-    vff = R.conj().T @ vf @ R
-    dff = R.conj().T @ df @ R
-    dff = np.real_if_close(dff)
-    vff = np.real_if_close(vff)
-    uff = np.real_if_close(uff)
-    return uff, dff, vff
-
-
-def symplectic_eigenvals(cov):
-    r"""Returns the symplectic eigenvalues of a covariance matrix.
-
-    Args:
-        cov (array): a covariance matrix
-
-    Returns:
-        (array): symplectic eigenvalues
-    """
-    M = int(len(cov) / 2)
-    D, _ = williamson(cov)
-    return np.diag(D)[:M]
-
-
-def takagi(A, rtol=1e-05, atol=1e-08, rounding=13, svd_order=True):
-    r"""Autonne-Takagi decomposition of a complex symmetric (not Hermitian!) matrix, this is the strawberryfields version.
-    Note that singular values of A are considered equal if they are equal after np.round(values, rounding).
-    See :cite:`cariolaro2016` and references therein for a derivation.
-    Args:
-        A (array[complex]): square, symmetric matrix
-        rounding (int): the number of decimal places to use when rounding the singular values of A
-        rtol (float): the relative tolerance parameter between ``A`` and ``A.T``
-        atol (float): the absolute tolerance parameter between ``A`` and ``A.T``
-        svd_order (boolean): whether to return result by ordering the singular values of ``A`` in descending (``True``) or asceding (``False``) order.
-    Returns:
-        tuple[array, array]: (rl, U), where rl are the (rounded) singular values,
-            and U is the Takagi unitary, such that :math:`N = U \diag(rl) U^T`.
-    """
-    # pylint: disable=too-many-statements,too-many-branches
-    n, m = A.shape
-    if n != m:
-        raise ValueError("The input matrix is not square")
-    if not np.allclose(A, A.T, rtol=rtol, atol=atol):
-        raise ValueError("The input matrix is not symmetric")
-
-    A = np.real_if_close(A)
-
-    if np.allclose(A, 0):
-        return np.zeros(n), np.eye(n)
-
-    if np.isrealobj(A):
-        # If the matrix A is real one can be more clever and use its eigendecomposition
-        ls, U = np.linalg.eigh(A)
-        U = U / np.exp(1j * np.angle(U)[0])
-        ls = np.round(ls, rounding)
-        vals = np.abs(ls)  # These are the Takagi eigenvalues
-        phases = -np.ones(vals.shape[0], dtype=np.complex128)
-        for j, l in enumerate(ls):
-            if np.allclose(l, 0) or l > 0:
-                phases[j] = 1
-        phases = np.sqrt(phases)
-        Uc = U @ np.diag(phases)  # One needs to readjust the phases
-        signs = np.sign(Uc.real)[0]
-        for k, s in enumerate(signs):
-            if np.allclose(s, 0):
-                signs[k] = 1
-        Uc = np.real_if_close(Uc / signs)
-        list_vals = [(vals[i], i) for i in range(len(vals))]
-        # And also rearrange the unitary and values so that they are decreasingly ordered
-        list_vals.sort(reverse=svd_order)
-        sorted_ls, permutation = zip(*list_vals)
-        return np.array(sorted_ls), Uc[:, np.array(permutation)]
-
-    phi = np.angle(A[0, 0])
-    Amr = np.real_if_close(np.exp(-1j * phi) * A)
-    if np.isrealobj(Amr):
-        vals, U = takagi(Amr, rtol=rtol, atol=atol, rounding=rounding, svd_order=svd_order)
-        return vals, U * np.exp(1j * phi / 2)
-
-    v, l, ws = np.linalg.svd(A)
-    if not svd_order:
-        v, l, ws = v[:, ::-1], l[::-1], ws[::-1, :]
-    w = ws.conj().T
-    rl = np.round(l, rounding)
-
-    # Generate list with degenerancies
-    result = []
-    for k, g in groupby(rl):
-        result.append(list(g))
-
-    # Generate lists containing the columns that correspond to degenerancies
-    kk = 0
-    for k in result:
-        for ind, _ in enumerate(k):
-            k[ind] = kk
-            kk = kk + 1
-
-    # Generate the lists with the degenerate column subspaces
-    vas = []
-    was = []
-    for i in result:
-        vas.append(v[:, i])
-        was.append(w[:, i])
-
-    # Generate the matrices qs of the degenerate subspaces
-    qs = []
-    for i in range(len(result)):
-        qs.append(sqrtm(np.transpose(vas[i]) @ was[i]))
-
-    # Construct the Takagi unitary
-    qb = block_diag(*qs)
-
-    U = v @ np.conj(qb)
-    return rl, U
 
 
 def williamson(V, rtol=1e-05, atol=1e-08):
@@ -300,3 +101,134 @@ def williamson(V, rtol=1e-05, atol=1e-08):
     Db = np.diag(dd + dd)
     S = Mm12 @ Ktt @ sqrtm(Db)
     return Db, np.linalg.inv(S).T
+
+
+def symplectic_eigenvals(cov):
+    r"""Returns the symplectic eigenvalues of a covariance matrix.
+
+    Args:
+        cov (array): a covariance matrix
+
+    Returns:
+        (array): symplectic eigenvalues
+    """
+    M = int(len(cov) / 2)
+    D, _ = williamson(cov)
+    return np.diag(D)[:M]
+
+
+def blochmessiah(S):
+    """Returns the Bloch-Messiah decomposition of a symplectic matrix S = uff @ dff @ vff
+       where uff and vff are orthogonal symplectic matrices and dff is a diagonal matrix
+       of the form diag(d1,d2,...,dn,d1^-1, d2^-1,...,dn^-1),
+
+    Args:
+        S (array[float]): 2N x 2N real symplectic matrix
+
+    Returns:
+        tuple(array[float],  : orthogonal symplectic matrix uff
+              array[float],  : diagonal matrix dff
+              array[float])  : orthogonal symplectic matrix vff
+    """
+
+    N, _ = S.shape
+
+    if not is_symplectic(S):
+        raise ValueError("Input matrix is not symplectic.")
+
+    # Changing Basis
+    R = (1 / np.sqrt(2)) * np.block(
+        [[np.eye(N // 2), 1j * np.eye(N // 2)], [np.eye(N // 2), -1j * np.eye(N // 2)]]
+    )
+    Sc = R @ S @ np.conjugate(R).T
+    # Polar Decomposition
+    u1, d1, v1 = np.linalg.svd(Sc)
+    Sig = u1 @ np.diag(d1) @ np.conjugate(u1).T
+    Unitary = u1 @ v1
+    # Blocks of Unitary and Hermitian symplectics
+    alpha = Unitary[0 : N // 2, 0 : N // 2]
+    beta = Sig[0 : N // 2, N // 2 : N]
+    # Bloch-Messiah in this Basis
+    d2, takagibeta = takagi(beta)
+    sval = np.arcsinh(d2)
+    uf = block_diag(takagibeta, takagibeta.conj())
+    blc = np.conjugate(takagibeta).T @ alpha
+    vf = block_diag(blc, blc.conj())
+    df = np.block(
+        [
+            [np.diag(np.cosh(sval)), np.diag(np.sinh(sval))],
+            [np.diag(np.sinh(sval)), np.diag(np.cosh(sval))],
+        ]
+    )
+    # Rotating Back to Original Basis
+    uff = np.conjugate(R).T @ uf @ R
+    vff = np.conjugate(R).T @ vf @ R
+    dff = np.conjugate(R).T @ df @ R
+    dff = np.real_if_close(dff)
+    vff = np.real_if_close(vff)
+    uff = np.real_if_close(uff)
+    return uff, dff, vff
+
+
+def takagi(A, svd_order=True):
+    r"""Autonne-Takagi decomposition of a complex symmetric (not Hermitian!) matrix.
+    Note that the input matrix is internally symmetrized. If the input matrix is indeed symmetric this leaves it unchanged.
+    See `Carl Caves note. <http://info.phys.unm.edu/~caves/courses/qinfo-s17/lectures/polarsingularAutonne.pdf>`_
+
+    Args:
+        A (array): square, symmetric matrix
+        svd_order (boolean): whether to return result by ordering the singular values of ``A`` in descending (``True``) or ascending (``False``) order.
+
+    Returns:
+        tuple[array, array]: (r, U), where r are the singular values,
+        and U is the Autonne-Takagi unitary, such that :math:`A = U \diag(r) U^T`.
+    """
+
+    n, m = A.shape
+    if n != m:
+        raise ValueError("The input matrix is not square")
+    # Here we force symmetrize the matrix
+    A = 0.5 * (A + A.T)
+
+    A = np.real_if_close(A)
+
+    if np.allclose(A, 0):
+        return np.zeros(n), np.eye(n)
+
+    if np.isrealobj(A):
+        # If the matrix A is real one can be more clever and use its eigendecomposition
+        ls, U = np.linalg.eigh(A)
+        U = U / np.exp(1j * np.angle(U)[0])
+        vals = np.abs(ls)  # These are the Takagi eigenvalues
+        phases = -np.ones(vals.shape[0], dtype=np.complex128)
+        for j, l in enumerate(ls):
+            if np.allclose(l, 0) or l > 0:
+                phases[j] = 1
+        phases = np.sqrt(phases)
+        Uc = U @ np.diag(phases)  # One needs to readjust the phases
+        signs = np.sign(Uc.real)[0]
+        for k, s in enumerate(signs):
+            if np.allclose(s, 0):
+                signs[k] = 1
+        Uc = np.real_if_close(Uc / signs)
+        list_vals = [(vals[i], i) for i in range(len(vals))]
+        # And also rearrange the unitary and values so that they are decreasingly ordered
+        list_vals.sort(reverse=svd_order)
+        sorted_ls, permutation = zip(*list_vals)
+        return np.array(sorted_ls), Uc[:, np.array(permutation)]
+
+    phi = np.angle(A[0, 0])
+    Amr = np.real_if_close(np.exp(-1j * phi) * A)
+    if np.isrealobj(Amr):
+        vals, U = takagi(Amr, svd_order=svd_order)
+        return vals, U * np.exp(1j * phi / 2)
+
+    u, d, v = np.linalg.svd(A)
+    U = u @ sqrtm((v @ np.conjugate(u)).T)
+    # The line above could be simplifed to the line below if the product v @ np.conjugate(u) is diagonal
+    # Which it should be according to Caves http://info.phys.unm.edu/~caves/courses/qinfo-s17/lectures/polarsingularAutonne.pdf
+    # U = u * np.sqrt(0j + np.diag(v @ np.conjugate(u)))
+    # This however breaks test_degenerate
+    if svd_order is False:
+        return d[::-1], U[:, ::-1]
+    return d, U
--- thewalrus/tests/test_decompositions.py
+++ thewalrus/tests/test_decompositions.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Xanadu Quantum Technologies Inc.
+# Copyright 2023 Xanadu Quantum Technologies Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,120 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Decomposition tests"""
-# pylint: disable=assignment-from-no-return
+# pylint: disable= assignment-from-no-return
 import numpy as np
 import pytest
 from scipy.linalg import block_diag
 
 from thewalrus.random import random_interferometer as haar_measure
 from thewalrus.random import random_symplectic
-from thewalrus.decompositions import autonne, blochmessiah, takagi, williamson
+from thewalrus.decompositions import williamson, blochmessiah, takagi
 from thewalrus.symplectic import sympmat as omega
 from thewalrus.quantum.gaussian_checks import is_symplectic
-
-
-class TestBlochMessiahDecomposition:
-    """Tests for the Bloch Messiah decomposition"""
-
-    @pytest.fixture
-    def create_transform(self):
-        """create a symplectic transform for use in testing.
-
-        Args:
-            n (int): number of modes
-            passive (bool): whether transform should be passive or not
-
-        Returns:
-            array: symplectic matrix
-        """
-
-        def _create_transform(n, passive=True):
-            """wrapped function"""
-            O = omega(n)
-
-            # interferometer 1
-            U1 = haar_measure(n)
-            S1 = np.vstack([np.hstack([U1.real, -U1.imag]), np.hstack([U1.imag, U1.real])])
-
-            Sq = np.identity(2 * n)
-            if not passive:
-                # squeezing
-                r = np.log(0.2 * np.arange(n) + 2)
-                Sq = block_diag(np.diag(np.exp(-r)), np.diag(np.exp(r)))
-
-            # interferometer 2
-            U2 = haar_measure(n)
-            S2 = np.vstack([np.hstack([U2.real, -U2.imag]), np.hstack([U2.imag, U2.real])])
-
-            # final symplectic
-            S_final = S2 @ Sq @ S1
-
-            # check valid symplectic transform
-            assert np.allclose(S_final.T @ O @ S_final, O)
-            return S_final
-
-        return _create_transform
-
-    @pytest.mark.parametrize("N", range(50, 500, 50))
-    def test_blochmessiah_rand(self, N):
-        """Tests blochmessiah function for different matrix sizes."""
-        S = random_symplectic(N)
-        u, d, v = blochmessiah(S)
-        assert np.allclose(u @ d @ v, S)
-        assert np.allclose(u.T @ u, np.eye(len(u)))
-        assert np.allclose(v.T @ v, np.eye(len(v)))
-        assert is_symplectic(u)
-        assert is_symplectic(v)
-
-    @pytest.mark.parametrize("M", [np.random.rand(4, 5), np.random.rand(4, 4)])
-    def test_blochmessiah_error(self, M):
-        """Tests that non-symplectic matrices raise a ValueError in blochmessiah."""
-        with pytest.raises(ValueError, match="Input matrix is not symplectic."):
-            blochmessiah(M)
-
-    def test_identity(self, tol):
-        """Test identity"""
-        n = 2
-        S_in = np.identity(2 * n)
-        O1, S, O2 = blochmessiah(S_in)
-
-        assert np.allclose(O1 @ O2, np.identity(2 * n), atol=tol, rtol=0)
-        assert np.allclose(S, np.identity(2 * n), atol=tol, rtol=0)
-
-        # test orthogonality
-        assert np.allclose(O1.T, O1, atol=tol, rtol=0)
-        assert np.allclose(O2.T, O2, atol=tol, rtol=0)
-
-        # test symplectic
-        O = omega(n)
-        assert np.allclose(O1 @ O @ O1.T, O, atol=tol, rtol=0)
-        assert np.allclose(O2 @ O @ O2.T, O, atol=tol, rtol=0)
-
-    @pytest.mark.parametrize("passive", [True, False])
-    def test_transform(self, passive, create_transform, tol):
-        """Test decomposition agrees with transform. also checks that passive transform has no squeezing.
-        Note: this test also tests the case with degenerate symplectic values"""
-        n = 3
-        S_in = create_transform(3, passive=passive)
-        O1, S, O2 = blochmessiah(S_in)
-
-        # test decomposition
-        assert np.allclose(O1 @ S @ O2, S_in, atol=tol, rtol=0)
-
-        # test no squeezing
-        if passive:
-            assert np.allclose(O1 @ O2, S_in, atol=tol, rtol=0)
-            assert np.allclose(S, np.identity(2 * n), atol=tol, rtol=0)
-
-        # test orthogonality
-        assert np.allclose(O1.T @ O1, np.identity(2 * n), atol=tol, rtol=0)
-        assert np.allclose(O2.T @ O2, np.identity(2 * n), atol=tol, rtol=0)
-
-        # test symplectic
-        O = omega(n)
-        assert np.allclose(O1.T @ O @ O1, O, atol=tol, rtol=0)
-        assert np.allclose(O2.T @ O @ O2, O, atol=tol, rtol=0)
-        assert np.allclose(S @ O @ S.T, O, atol=tol, rtol=0)
 
 
 class TestWilliamsonDecomposition:
@@ -255,37 +151,108 @@ class TestWilliamsonDecomposition:
         assert np.allclose(S @ O @ S.T, O, atol=tol, rtol=0)
 
 
-@pytest.mark.parametrize("n", [5, 10, 50])
-@pytest.mark.parametrize("datatype", [np.complex128, np.float64])
-@pytest.mark.parametrize("svd_order", [True, False])
-def test_autonne(n, datatype, svd_order):
-    """Checks the correctness of the Autonne decomposition function"""
-    if datatype is np.complex128:
-        A = np.random.rand(n, n) + 1j * np.random.rand(n, n)
-    if datatype is np.float64:
-        A = np.random.rand(n, n)
-    A += A.T
-    r, U = autonne(A, svd_order=svd_order)
-    assert np.allclose(A, U @ np.diag(r) @ U.T)
-    assert np.all(r >= 0)
-    if svd_order is True:
-        assert np.all(np.diff(r) <= 0)
-    else:
-        assert np.all(np.diff(r) >= 0)
+class TestBlochMessiahDecomposition:
+    """Tests for the Bloch Messiah decomposition"""
 
+    @pytest.fixture
+    def create_transform(self):
+        """create a symplectic transform for use in testing.
 
-def test_autonne_error():
-    """Tests the value errors of Autonne"""
-    n = 10
-    m = 20
-    A = np.random.rand(n, m)
-    with pytest.raises(ValueError, match="The input matrix is not square"):
-        autonne(A)
-    n = 10
-    m = 10
-    A = np.random.rand(n, m)
-    with pytest.raises(ValueError, match="The input matrix is not symmetric"):
-        autonne(A)
+        Args:
+            n (int): number of modes
+            passive (bool): whether transform should be passive or not
+
+        Returns:
+            array: symplectic matrix
+        """
+
+        def _create_transform(n, passive=True):
+            """wrapped function"""
+            O = omega(n)
+
+            # interferometer 1
+            U1 = haar_measure(n)
+            S1 = np.vstack([np.hstack([U1.real, -U1.imag]), np.hstack([U1.imag, U1.real])])
+
+            Sq = np.identity(2 * n)
+            if not passive:
+                # squeezing
+                r = np.log(0.2 * np.arange(n) + 2)
+                Sq = block_diag(np.diag(np.exp(-r)), np.diag(np.exp(r)))
+
+            # interferometer 2
+            U2 = haar_measure(n)
+            S2 = np.vstack([np.hstack([U2.real, -U2.imag]), np.hstack([U2.imag, U2.real])])
+
+            # final symplectic
+            S_final = S2 @ Sq @ S1
+
+            # check valid symplectic transform
+            assert np.allclose(S_final.T @ O @ S_final, O)
+            return S_final
+
+        return _create_transform
+
+    @pytest.mark.parametrize("N", range(50, 500, 50))
+    def test_blochmessiah_rand(self, N):
+        """Tests blochmessiah function for different matrix sizes."""
+        S = random_symplectic(N)
+        u, d, v = blochmessiah(S)
+        assert np.allclose(u @ d @ v, S)
+        assert np.allclose(u.T @ u, np.eye(len(u)))
+        assert np.allclose(v.T @ v, np.eye(len(v)))
+        assert is_symplectic(u)
+        assert is_symplectic(v)
+
+    @pytest.mark.parametrize("M", [np.random.rand(4, 5), np.random.rand(4, 4)])
+    def test_blochmessiah_error(self, M):
+        """Tests that non-symplectic matrices raise a ValueError in blochmessiah."""
+        with pytest.raises(ValueError, match="Input matrix is not symplectic."):
+            blochmessiah(M)
+
+    def test_identity(self, tol):
+        """Test identity"""
+        n = 2
+        S_in = np.identity(2 * n)
+        O1, S, O2 = blochmessiah(S_in)
+
+        assert np.allclose(O1 @ O2, np.identity(2 * n), atol=tol, rtol=0)
+        assert np.allclose(S, np.identity(2 * n), atol=tol, rtol=0)
+
+        # test orthogonality
+        assert np.allclose(O1.T, O1, atol=tol, rtol=0)
+        assert np.allclose(O2.T, O2, atol=tol, rtol=0)
+
+        # test symplectic
+        O = omega(n)
+        assert np.allclose(O1 @ O @ O1.T, O, atol=tol, rtol=0)
+        assert np.allclose(O2 @ O @ O2.T, O, atol=tol, rtol=0)
+
+    @pytest.mark.parametrize("passive", [True, False])
+    def test_transform(self, passive, create_transform, tol):
+        """Test decomposition agrees with transform. also checks that passive transform has no squeezing.
+        Note: this test also tests the case with degenerate symplectic values"""
+        n = 3
+        S_in = create_transform(3, passive=passive)
+        O1, S, O2 = blochmessiah(S_in)
+
+        # test decomposition
+        assert np.allclose(O1 @ S @ O2, S_in, atol=tol, rtol=0)
+
+        # test no squeezing
+        if passive:
+            assert np.allclose(O1 @ O2, S_in, atol=tol, rtol=0)
+            assert np.allclose(S, np.identity(2 * n), atol=tol, rtol=0)
+
+        # test orthogonality
+        assert np.allclose(O1.T @ O1, np.identity(2 * n), atol=tol, rtol=0)
+        assert np.allclose(O2.T @ O2, np.identity(2 * n), atol=tol, rtol=0)
+
+        # test symplectic
+        O = omega(n)
+        assert np.allclose(O1.T @ O @ O1, O, atol=tol, rtol=0)
+        assert np.allclose(O2.T @ O @ O2, O, atol=tol, rtol=0)
+        assert np.allclose(S @ O @ S.T, O, atol=tol, rtol=0)
 
 
 @pytest.mark.parametrize("n", [5, 10, 50])
@@ -307,15 +274,123 @@ def test_takagi(n, datatype, svd_order):
         assert np.all(np.diff(r) >= 0)
 
 
+@pytest.mark.parametrize("n", [5, 10, 50])
+@pytest.mark.parametrize("datatype", [np.complex128, np.float64])
+@pytest.mark.parametrize("svd_order", [True, False])
+@pytest.mark.parametrize("half_rank", [0, 1])
+@pytest.mark.parametrize("phase", [0, 1])
+def test_degenerate(n, datatype, svd_order, half_rank, phase):
+    """Tests Takagi produces the correct result for very degenerate cases"""
+    nhalf = n // 2
+    diags = [half_rank * np.random.rand()] * nhalf + [np.random.rand()] * (n - nhalf)
+    if datatype is np.complex128:
+        U = haar_measure(n)
+    if datatype is np.float64:
+        U = np.exp(1j * phase) * haar_measure(n, real=True)
+    A = U @ np.diag(diags) @ U.T
+    r, U = takagi(A, svd_order=svd_order)
+    assert np.allclose(A, U @ np.diag(r) @ U.T)
+    assert np.allclose(U @ U.T.conj(), np.eye(n))
+    assert np.all(r >= 0)
+    if svd_order is True:
+        assert np.all(np.diff(r) <= 0)
+    else:
+        assert np.all(np.diff(r) >= 0)
+
+
+def test_zeros():
+    """Verify that the Takagi decomposition returns a zero vector and identity matrix when
+    input a matrix of zeros"""
+    dim = 4
+    a = np.zeros((dim, dim))
+    rl, U = takagi(a)
+    assert np.allclose(rl, np.zeros(dim))
+    assert np.allclose(U, np.eye(dim))
+
+
 def test_takagi_error():
     """Tests the value errors of Takagi"""
     n = 10
-    m = 20
+    m = 11
     A = np.random.rand(n, m)
     with pytest.raises(ValueError, match="The input matrix is not square"):
         takagi(A)
-    n = 10
-    m = 10
-    A = np.random.rand(n, m)
-    with pytest.raises(ValueError, match="The input matrix is not symmetric"):
-        takagi(A)
+
+
+def test_real_degenerate():
+    """Verify that the Takagi decomposition returns a matrix that is unitary and results in a
+    correct decomposition when input a real but highly degenerate matrix. This test uses the
+    adjacency matrix of a balanced tree graph."""
+
+    vals = [
+        1,
+        2,
+        31,
+        34,
+        35,
+        62,
+        67,
+        68,
+        94,
+        100,
+        101,
+        125,
+        133,
+        134,
+        157,
+        166,
+        167,
+        188,
+        199,
+        200,
+        220,
+        232,
+        233,
+        251,
+        265,
+        266,
+        283,
+        298,
+        299,
+        314,
+        331,
+        332,
+        346,
+        364,
+        365,
+        377,
+        397,
+        398,
+        409,
+        430,
+        431,
+        440,
+        463,
+        464,
+        472,
+        503,
+        535,
+        566,
+        598,
+        629,
+        661,
+        692,
+        724,
+        755,
+        787,
+        818,
+        850,
+        881,
+        913,
+        944,
+    ]
+    mat = np.zeros([31 * 31])
+    mat[vals] = 1
+    mat = mat.reshape(31, 31)
+    # The lines above are equivalent to:
+    # import networkx as nx
+    # g = nx.balanced_tree(2, 4)
+    # a = nx.to_numpy_array(g)
+    rl, U = takagi(mat)
+    assert np.allclose(U @ U.conj().T, np.eye(len(mat)))
+    assert np.allclose(U @ np.diag(rl) @ U.T, mat)
