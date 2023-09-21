5e621aaf9efc70b3ed83baec5645fbb3cc6e2c46
Nicolas Quesada <nquesada@pop-os.localdomain>
2023-09-20 21:40:11 -0400

    Passes black

--- thewalrus/reference.py
+++ thewalrus/reference.py
@@ -332,7 +332,12 @@ def rpmp(s):
         generator: the set of restricted perfect matching permutations of the tuple s
     """
     m = len(s) // 2
-    local_mapper = lambda x: mapper(x, s)
+    def local_mapper(x):
+        """Helper function to define a local mapper based on the symbols s
+            Args:
+                x (iterable): object to be mapped
+        """
+        return mapper(x, s)
     for i in product(permutations(range(1, m)), bitstrings(m)):
         yield local_mapper(i)
 
