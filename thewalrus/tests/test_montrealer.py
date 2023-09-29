65ac51e25258a699041d5a27f40ba969b4a53122
yaniccd <yanic.cardin.1@gmail.com>
2023-09-28 21:06:43 -0400

    mtl first tests

--- thewalrus/tests/test_montrealer.py
+++ thewalrus/tests/test_montrealer.py
@@ -1,10 +1,14 @@
 import pytest
 import numpy as np
-from thewalrus import mtl
+from thewalrus import mtl, lmtl
+from thewalrus._montrealer import montrealer, lmontrealer
+from thewalrus.reference import rspm, rpmp
+#from thewalrus.quantum import
 from scipy.special import factorial2
 
 @pytest.mark.parametrize("n", list(range(1,6)))
 def test_montrealer_all_ones(n):
+    """Test that the Montrealer of a matrix of ones gives (2n-2)!!"""
     A = np.ones([2*n, 2*n])
     mtl_val = mtl(A)
     mtl_expect = factorial2(2*n-2)
