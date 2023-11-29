8ec2172e5bdb48a7d32886742fe3e7c829e48fd9
Nicolas Quesada <991946+nquesada@users.noreply.github.com>
2023-11-29 15:53:11 -0500

    Implements the Montrealer (#374)
    * Saving changes
    
    * Passes black
    
    * Passes black
    
    * Black
    
    * updates acknowledgements and changelog
    
    * updates acknowledgements and changelog
    
    * Adds montrealer
    
    * Adds Montrealer tests (#375)
    
    * Yanic the pirate
    
    * mtl test
    
    * mtl first tests
    
    * mtl test
    
    * test mtl
    
    * test montrealer
    
    * test montrealer
    
    * test Montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * tests montrealer
    
    * montrealer test
    
    * montrealer test
    
    * montrealer test
    
    * montrealer test
    
    * montrealer test
    
    * test montrealer
    
    * test montrealer
    
    * montrealer test
    
    * montrealer test
    
    * montrealer test
    
    * montrealer tests
    
    * montrealer tests
    
    * montrealer tests
    
    * montrealer tests
    
    * functions header update
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer ready for review
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer - failed tests
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * test montrealer
    
    * the rspms are now ordered
    
    * Passes black
    
    * garbage test file to be deleted later
    
    * minor changes
    
    * Done
    
    ---------
    
    Co-authored-by: Nicolas Quesada <nquesada@pop-os.localdomain>
    
    * making bots happy
    
    * Apply suggestions from code review
    
    Co-authored-by: Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
    
    * Adds extra tests
    
    ---------
    
    Co-authored-by: Nicolas Quesada <nquesada@pop-os.localdomain>
    Co-authored-by: Yanic Cardin <yanic.cardin.1@gmail.com>
    Co-authored-by: Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>

--- thewalrus/__init__.py
+++ thewalrus/__init__.py
@@ -132,6 +132,12 @@ from ._torontonian import (
     rec_torontonian,
     rec_ltorontonian,
 )
+
+from ._montrealer import (
+    mtl,
+    lmtl,
+)
+
 from ._version import __version__
 
 
@@ -152,6 +158,8 @@ __all__ = [
     "reduction",
     "hermite_multidimensional",
     "grad_hermite_multidimensional",
+    "mtl",
+    "lmtl",
     "version",
 ]
 
