f7646b38b7c5bb054c12c8a53e4be7f63cddb2aa
Nicolas Quesada <nquesada@pop-os.localdomain>
2024-01-26 16:32:37 -0500

    Unnecesary import

--- thewalrus/decompositions.py
+++ thewalrus/decompositions.py
@@ -35,7 +35,7 @@ Code details
 """
 import numpy as np
 
-from scipy.linalg import block_diag, sqrtm, schur, polar
+from scipy.linalg import sqrtm, schur, polar
 from thewalrus.symplectic import sympmat
 from thewalrus.quantum.gaussian_checks import is_symplectic
 
