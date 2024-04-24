62cd625b18ec602e5ee15f71cf8dd9dcad2c9f3b
Nicolas Quesada <nquesada@pop-os.localdomain>
2024-04-24 10:20:44 -0400

    some tests pass

--- thewalrus/internal_modes/fock_density_matrices.py
+++ thewalrus/internal_modes/fock_density_matrices.py
@@ -196,41 +196,37 @@ def density_matrix_single_mode(
         dm = np.zeros([cutoff, cutoff], dtype=np.complex128)
         num_modes = M * K
         block_size = K
-        for i in range(cutoff):
-            if method == "diagonals":
-                lower_limit = i
-            else:
-                lower_limit = 0
-            for j in range(lower_limit, i + 1):
-                if method == "non-recursive":
-                    if (i - j) % 2 == 0:
-                        patt_long = list((j,) + tuple(N_nums) + ((i - j) // 2,))
-                        new_blocks = np.concatenate((blocks, np.array([K + blocks[-1]])), axis=0)
-                        perm = (
-                            list(range(num_modes))
-                            + list(range(block_size))
-                            + list(range(num_modes, 2 * num_modes))
-                            + list(range(block_size))
-                        )
-                        Aperm = A[:, perm][perm]
-                        dm[j, i] = (
-                            pref
-                            * haf_blocked(Aperm, blocks=new_blocks, repeats=patt_long)
-                            / (
-                                np.prod(factorial(patt_long[1:-1]))
-                                * np.sqrt(factorial(i) * factorial(j))
+        if method == "non-recursive":
+            for i in range(cutoff):
+                for j in range(i + 1):
+                        if (i - j) % 2 == 0:
+                            patt_long = list((j,) + tuple(N_nums) + ((i - j) // 2,))
+                            new_blocks = np.concatenate((blocks, np.array([K + blocks[-1]])), axis=0)
+                            perm = (
+                                list(range(num_modes))
+                                + list(range(block_size))
+                                + list(range(num_modes, 2 * num_modes))
+                                + list(range(block_size))
                             )
-                        )
-                        dm[i, j] = np.conj(dm[j, i])
-                    else:
-                        dm[i, j] = 0
-                        dm[j, i] = 0
-                if method == "diagonals":
-                    patt_long = (i,) + tuple(N_nums)
-                    dm[i, i] = pref * np.real(
-                        haf_blocked(A, blocks=blocks, repeats=patt_long)
-                        / np.prod(factorial(patt_long))
-                    )
+                            Aperm = A[:, perm][perm]
+                            dm[j, i] = (
+                                pref
+                                * haf_blocked(Aperm, blocks=new_blocks, repeats=patt_long)
+                                / (
+                                    np.prod(factorial(patt_long[1:-1]))
+                                    * np.sqrt(factorial(i) * factorial(j))
+                                )
+                            )
+                            dm[i, j] = np.conj(dm[j, i])
+                        else:
+                            dm[i, j] = 0
+                            dm[j, i] = 0
+        for i in range(cutoff):
+            patt_long = (i,) + tuple(N_nums)
+            dm[i, i] = pref * np.real(
+                haf_blocked(A, blocks=blocks, repeats=patt_long)
+                / np.prod(factorial(patt_long))
+            )
         if normalize:
             dm = dm / np.trace(dm)
         return dm
